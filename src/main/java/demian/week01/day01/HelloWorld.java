e5ae5d5ee77eadb0ae7bbefa802e0a23a3b8b926
Demian <demian.fitz@gmail.com>
2024-02-26 11:00:47 +0100

    - Changed HelloWorld to Helloworld2

--- src/main/java/demian/week01/day01/HelloWorld.java
+++ src/main/java/demian/week01/day01/HelloWorld.java
@@ -2,6 +2,6 @@ package demian.week01.day01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
