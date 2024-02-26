9c35a36df2ad31ceb0e0a70418a9ea4ecf95aed0
Demian <demian.fitz@gmail.com>
2024-02-26 17:54:51 +0100

    - Diverses

---
+++ src/main/java/demian/week01/day01/Sandkasten.java
@@ -0,0 +1,11 @@
+package demian.week01.day01;
+
+public class Sandkasten {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Iteration: " + i);
+            i++;
+        } while (i < 5);
+    }
+}
