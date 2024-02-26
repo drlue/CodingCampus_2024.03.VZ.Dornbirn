9164a960edfab084b520766cb951f7a83d0956e6
Ardijan <Ardijanla_Kajtazila@hotmail.com>
2024-02-26 14:20:00 +0100

    Aufgabe: Zählen Zähle in einer Schleife von 0 bis inklusive 10 und gib die Zahlen aus.
    Verwende dazu eine for-Schleife
    Verwende dazu eine while-Schleife und erstelle eine separate Zählvariable
    Aufgabe: Zählen 2
    Zähle in einer Schleife von 0 bis exklusive 10 und gib die Zählvariable aus.
    
    Aufgabe: Zählen mit Trick
    Zähle in einer Schleife von 0 bis inklusive 10 gib nur jede gerade Zahl aus.
    
    Verwende um zu prüfen ob es eine gerade Zahl ist den Modulo Operator
    Modifiziere die Zählvariable so, dass die Zählvariable nur jede gerade Zahlen enthält
    Aufgabe: Rewind
    Zähle in einer Schleife rückwärts von 10 bis inklusive -10 und gib die Zählvariable aus.

---
+++ src/main/java/ardijanla/week01/day01/ÜbungZählen.java
@@ -0,0 +1,44 @@
+package ardijanla.week01.day01;
+
+public class ÜbungZählen {
+
+    public static void main(String[] args) {
+
+        CountToFor(10);
+        CountToWhile(10,true,true);
+        CountBackwards(10);
+
+    }
+
+
+    public static void  CountToFor (int numberToCount){
+
+        for (int i = 0; i <= numberToCount; i++) {
+            System.out.println("number: " + i);
+        }
+    }
+
+    public static void CountToWhile(int numberToCount,boolean exklusiveLastNumber,boolean OnlyOdd){
+        if (!exklusiveLastNumber) {
+            numberToCount--;
+        }
+
+        int counter = 0;
+        while (counter <= numberToCount ){
+
+            if (counter % 2 == 0 && OnlyOdd){
+
+                System.out.println("number: " + counter);
+            } else if (!OnlyOdd) {
+                System.out.println("odd number: " + counter);
+            }
+            counter++;
+        }
+    }
+
+    public static void CountBackwards(int numberToCount){
+        for (int i = numberToCount; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+}
