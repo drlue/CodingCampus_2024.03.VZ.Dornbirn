b3e818fabfb75bcbb08fe187b87f93dc24463888
Christian Schmid <schmid100@hotmail.com>
2024-02-26 14:06:28 +0100

    - Added Hello World! - Variablen(primitiv Datatypes) - Schleifen/Loops

---
+++ src/main/java/christian/week01/day01/Loops.java
@@ -0,0 +1,33 @@
+package christian.week01.day01;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        //Aufgabe Zählen for
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //Aufgabe Zählen while
+        int index = 0;
+        while (index < 10) {
+            index++;
+        }
+        //Aufgabe Zählen mit Trick
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        //Aufgabe Rewind
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        System.out.println(ZweitePotenz(5));
+    }
+
+    public static int ZweitePotenz(int a) {
+        int potenz = a * a;
+        return potenz;
+    }
+}
