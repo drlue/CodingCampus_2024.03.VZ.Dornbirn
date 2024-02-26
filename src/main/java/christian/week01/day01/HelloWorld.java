b3f911c3f8aee7880b12c48deef340c28f316e16
Christian Schmid <schmid100@hotmail.com>
2024-02-26 11:03:12 +0100

    - Added Hello World!

--- src/main/java/christian/week01/day01/HelloWorld.java
+++ src/main/java/christian/week01/day01/HelloWorld.java
@@ -3,7 +3,7 @@ package christian.week01.day01;
 public class HelloWorld
 {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.print("Hello World!");
         System.out.print("zweite Nachricht");
     }
 }
