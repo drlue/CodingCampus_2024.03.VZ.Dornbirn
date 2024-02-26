3b9bcb16988110b1700f5f45c932bb2c4bd296cd
Ardijan <Ardijanla_Kajtazila@hotmail.com>
2024-02-26 14:57:51 +0100

    - Removed Ü and Ä.

--- src/main/java/ardijanla/week01/day01/UebungZaehlen.java
+++ src/main/java/ardijanla/week01/day01/UebungZaehlen.java
@@ -1,32 +1,32 @@
 package ardijanla.week01.day01;
 
-public class ÜbungZählen {
+public class UebungZaehlen {
 
     public static void main(String[] args) {
 
         CountToFor(10);
-        CountToWhile(10,true,true);
+        CountToWhile(10, true, true);
         CountBackwards(10);
 
     }
 
 
-    public static void  CountToFor (int numberToCount){
+    public static void CountToFor(int numberToCount) {
 
         for (int i = 0; i <= numberToCount; i++) {
             System.out.println("number: " + i);
         }
     }
 
-    public static void CountToWhile(int numberToCount,boolean exklusiveLastNumber,boolean OnlyOdd){
+    public static void CountToWhile(int numberToCount, boolean exklusiveLastNumber, boolean OnlyOdd) {
         if (!exklusiveLastNumber) {
             numberToCount--;
         }
 
         int counter = 0;
-        while (counter <= numberToCount ){
+        while (counter <= numberToCount) {
 
-            if (counter % 2 == 0 && OnlyOdd){
+            if (counter % 2 == 0 && OnlyOdd) {
 
                 System.out.println("number: " + counter);
             } else if (!OnlyOdd) {
@@ -36,7 +36,7 @@ public class ÜbungZählen {
         }
     }
 
-    public static void CountBackwards(int numberToCount){
+    public static void CountBackwards(int numberToCount) {
         for (int i = numberToCount; i >= -10; i--) {
             System.out.println(i);
         }
