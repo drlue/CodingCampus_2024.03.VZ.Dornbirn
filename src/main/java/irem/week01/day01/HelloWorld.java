ed83427b3e3cde3b9d413caa8adde6af34333cd1
iremnuraksoy <aksoy.irem5757@gamil.com>
2024-02-26 11:14:49 +0100

    Added Hallo World

--- src/main/java/irem/week01/day01/HelloWorld.java
+++ src/main/java/irem/week01/day01/HelloWorld.java
@@ -2,8 +2,7 @@ package irem.week01.day01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printl("hello back");
+        System.out.println("hello back");
 
     }
 }
-
