0aa6b2d7150d8fbc1ab8eeb1c7515cd42f4b3614
iremnuraksoy <aksoy.irem5757@gamil.com>
2024-02-27 10:27:24 +0100

    Schleife 4 in Anfang

--- src/main/java/irem/Schleifen2.java
+++ src/main/java/irem/Schleifen2.java
@@ -3,10 +3,7 @@ package irem;
 public class Schleifen2 {
     public static void main(String[] args) {
         for (int counter = 0; counter <= 10; counter++) {
-            System.out.println("Unsere Counter" + counter);
-            System.out.println("Unsere Counter" + counter);
-            counter++;
-            System.out.println("Unsere Counter + counter");
+            System.out.println("Unsere Counter " + counter);
         }
     }
 
