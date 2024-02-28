a55955adb33e070ecacbfc2e0708f92e72c2ebb1
schmid <schmid100@hotmail.com>
2024-02-28 20:47:50 +0100

    - EmptySquare-String-Rückgabe mit Stringbuilder

--- src/main/java/christian/week01/day01/StringBuilder.java
+++ src/main/java/christian/week01/day01/StringBuilder.java
@@ -1,21 +1,23 @@
-
 package christian.week01.day01;
 
 public class StringBuilder {
 
-public static void main(String[] args) {
-    System.out.println(printEmptyXSquare("x", 10));
+    public static void main(String[] args) {
+
+        System.out.println(printEmptyXSquare("x", 15));
     }
-    // leeres Quadrat ausgeben
+
+    // leeres Quadrat ausgeben mit Stringbuilder .append und .insert (int offset, string)
     public static String printEmptyXSquare(String xChar, int count) {
-    String solution;
+        String solution;
         java.lang.StringBuilder solutionB = new java.lang.StringBuilder();
         for (int row = 0; row < count; row++) {
             for (int col = 0; col < count; col++) {
                 if (row == 0 || col == 0 || row == count - 1 || col == count - 1) {
-                   solutionB.append(xChar);
+                    solutionB.append(xChar);
+                    solutionB.append(' ');
                 } else {
-                   solutionB.insert(solutionB.length()," ");
+                    solutionB.insert(solutionB.length(), "  ");
                 }
             }
             solutionB.append("\n");
