54eaef9fa4bfd8a08e99cde8d0bbfdfa4447c764
Aza2019 <vincentjb07@hotmail.com>
2024-02-29 00:22:29 +0100

    -X completed -Rhombus completed

--- src/main/java/franco/week01/day01/AufgabePrint.java
+++ src/main/java/franco/week01/day01/AufgabePrint.java
@@ -17,13 +17,17 @@ public class AufgabePrint {
 
         printEmptySquare("+ ", 10);
 
-        printEmptySquare("A ",3);
+        printEmptySquare("A ", 3);
 
         printSlash("X", 6, true);
 
-        printSlash2("Y",5,true);
+        printSlash2("Y", 5, true);
 
-        printTri("X",4);
+        printTri("X", 5);
+
+        printX("X", 5);
+
+        printRhombus("x", 7);
 
 
     }
@@ -60,8 +64,8 @@ public class AufgabePrint {
 
     //Print a triangle right to left
     public static void printTriangleBottomLeft(String text, int number) {
-        for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print(text);
             }
             System.out.println();
@@ -94,29 +98,28 @@ public class AufgabePrint {
 
     }
 
-    public static void printEmptySquare(String text, int number){
-        for (int i = 1; i <= number; i++)  {
-            for (int j = 1; j<= number; j++) {
-                if (i==1||j==1||i==number||j==number){
-                    System.out.print(text);}
-                else {
+    public static void printEmptySquare(String text, int number) {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number; j++) {
+                if (i == 1 || j == 1 || i == number || j == number) {
+                    System.out.print(text);
+                } else {
                     System.out.print("  ");
                 }
 
-            }System.out.println();
+            }
+            System.out.println();
 
         }
 
     }
-    public static void printSlash(String text,int number,boolean isSlashOrNot){
+
+    public static void printSlash(String text, int number, boolean isSlashOrNot) {
         for (int i = 0; i < number; i++) {
-            for (int j = 0; j < number; j++)
-            {
-                if (i==j)
-                {
+            for (int j = 0; j < number; j++) {
+                if (i == j) {
                     System.out.print(text);
-                }
-                else {
+                } else {
                     System.out.print(" ");
 
                 }
@@ -128,33 +131,45 @@ public class AufgabePrint {
         }
     }
 
-    public static void printSlash2(String text,int size,boolean isSlashOrNot){
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++)
-            {
-                if (i + j == size - 1)
-                {
-                    System.out.print(text);
-                }
-                else {
-                    System.out.print(".");
-                }
-
-            }
-
-            System.out.println( );
-        }
-    }
-     //Code  not functioning properly-Modification to be made
-    public static void printTri(String text,int size){
+    public static void printSlash2(String text, int size, boolean isSlashOrNot) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-
-                if (i+j == size ||  i==j){
-                    System.out.println(text);
-                }
-                else {
+                if (i + j == size - 1) {
+                    System.out.print(text);
+                } else {
                     System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void printTri(String text, int size) {
+        for (int row = 1; row <= size; row++) {
+            for (int column = 1; column <= 2 * size - 1; column++) {
+                if (row == size || row + column == size + 1 || column == row + (size - 1)) {
+                    System.out.print(text);
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+
+    public static void printX(String text, int size) {
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+
+                if (row + column == size - 1 || row == column) {
+                    System.out.print(text);
+                } else {
+                    System.out.print(" ");
                 }
 
             }
@@ -163,5 +178,21 @@ public class AufgabePrint {
 
     }
 
+    public static void printRhombus(String text, int size) {
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+                if (column == (size / 2) - row || column == row - (size / 2)
+                        || column == row + (size / 2) ||
+                        column == (3 * (size / 2)) - row) {
+
+                    System.out.print("A");
+
+                } else {
+                    System.out.print("_");
+                }
+            }
+            System.out.println("_");
+        }
+    }
 
 }
