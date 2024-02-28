57e811f8418a802d83434b80d1ab18165db86c29
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-02-28 08:37:15 +0100

    - Added a new class for today's lessons - added the last triangle to my collection of triangles

--- src/main/java/katherina/week01/day02AndDay03/AufgabeMethodenUndSchleifen.java
+++ src/main/java/katherina/week01/day02AndDay03/AufgabeMethodenUndSchleifen.java
@@ -1,17 +1,22 @@
 package katherina.week01.day02AndDay03;
 
-public class AufgabePrintChars {
+public class AufgabeMethodenUndSchleifen {
     public static void main(String[] args) {
+        System.out.println("\nprintChars");
         printChars("X", 10);
+        System.out.println("\nprintSquare");
         printSquare("X", 10);
+        System.out.println("\nprintRectangle");
         printRectangle("x", 10, 3);
+        System.out.println("\nprintTriangleBottomLeft");
         printTriangleBottomLeft("x", 4);
         System.out.println("\nprintTriangleTopLeft");
         printTriangleTopLeft("x", 5);
         System.out.println("\nprintTriangleTopRight");
         printTriangleTopRight("x ", 5);
-        System.out.println("\nprintTriangleTopLeft2");
-        printTriangleTopLeft2("x ", 5);
+        System.out.println("\nprintTriangleBottomRight");
+        printTriangleBottomRight("x ", 5);
+
     }
 //Vor dem Schlaf und nach der Methodendefinition Semikolon nicht vergessen!
 
@@ -22,20 +27,16 @@ public class AufgabePrintChars {
             System.out.print(text);
             ++i;
         }
-        System.out.println();
-        System.out.println();
     }
 
     //Ich erzeuge ein "Quadrat" (gleichviele Zeilen und Zeichen, naja, theoretisch)
     public static void printSquare(String text, int size) {
         for (int reihe = 0; reihe < size; reihe++) {
-            for (int buchstabe = 0; buchstabe <= 10; buchstabe++) {
+            for (int buchstabe = 0; buchstabe < size; buchstabe++) {
                 System.out.print(text);
             }
-            System.out.println(text);
+            System.out.println();
         }
-        System.out.println();
-        System.out.println();
     }
 
 
@@ -50,8 +51,6 @@ public class AufgabePrintChars {
             }
             System.out.println(text);
         }
-        System.out.println();
-        System.out.println();
     }
     //Merksatz: Klammern sind Freunde! Kein Futter! (Nicht aufessen, sondern setzen!)
 
@@ -64,8 +63,6 @@ public class AufgabePrintChars {
             }
             System.out.println();
         }
-        System.out.println();
-        System.out.println();
     }
 //Merke: Aufpassen! Was soll denn größer als was sein? In diesem Fall: Die Zahl der Reihen kleiner gleich der int Variable (also maximal vier).
     //Und ab der ersten Reihe nach der nullten Reihe soll immer ein Buchstabe hinzukommen. Also erst ab buchstabe = eins wird was hinzugefügt.
@@ -83,32 +80,33 @@ public class AufgabePrintChars {
 
     //Und nun soll das gleiche Dreieck erzeugt werden, aber ... gespiegelt.
 
-    public static void printTriangleTopLeft2(String text, int size) {
+    public static void printTriangleTopRight(String text, int size) {
         for (int reihe = 0; size > reihe; reihe++) {
-            for (int buchstabe = size - 1; reihe <= buchstabe; buchstabe--) {
-                System.out.print(text);
-                {
-                    for (buchstabe = reihe + 1; buchstabe < size; buchstabe++) {
-                        System.out.print(". ");
-                    }
-                    System.out.println();
-                }
+            for (int buchstabe = 0; buchstabe < reihe; buchstabe++) {
+                System.out.print(". ");
             }
+            for (int buchstabe = reihe + 1; buchstabe < size; buchstabe++) {
+                System.out.print(text);
+            }
+            System.out.println();
         }
     }
 
-    public static void printTriangleTopRight(String text, int size) {
+//Letztes Dreieck! Das entstand eigentlich an Tag 03, aber ich will alle Dreiecke in einer Datei, um die Codes vergleichen zu können!
+
+    public static void printTriangleBottomRight(String text, int size) {
         for (int reihe = 0; size > reihe; reihe++) {
-            for (int buchstabe = 0; buchstabe <= reihe; buchstabe++) {
-                System.out.print(text);
-            }
             for (int buchstabe = reihe + 1; buchstabe < size; buchstabe++) {
                 System.out.print(". ");
+            }
+            for (int buchstabe = 0; buchstabe < reihe; buchstabe++) {
+                System.out.print(text);
             }
             System.out.println();
         }
     }
 }
-
+//Merke: Ich kann auf zwei Weisen steuern, welches Zeichen zuerst gedruckt wird. Erstens, indem ich festlege, welche Schleife was druckt.
+//Zweitens, indem ich festlege, welche der For-Schleifen zuerst ausgeführt wird. Alle Dreiecke sollen genau darauf angeschaut werden.
 
 
