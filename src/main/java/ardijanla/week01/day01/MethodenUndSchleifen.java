0a1041a6b8a27073fed92c40ac647bbe7a70ac9e
Ardijan <Ardijanla_Kajtazila@hotmail.com>
2024-02-29 11:07:57 +0100

    -Mehtoden Schleifen final

--- src/main/java/ardijanla/week01/day01/MethodenUndSchleifen.java
+++ src/main/java/ardijanla/week01/day01/MethodenUndSchleifen.java
@@ -27,7 +27,6 @@ public class MethodenUndSchleifen {
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < number; i++) {
             output.append(character);
-
         }
         System.out.println(output);
         return output.toString();
