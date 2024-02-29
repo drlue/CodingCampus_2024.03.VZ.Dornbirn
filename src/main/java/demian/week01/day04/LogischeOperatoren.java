c5de13fdc93ebbd5d8591d8c5a8dc17b99ab00cc
Demian <demian.fitz@gmail.com>
2024-02-29 11:29:20 +0100

    - Class für logische Operatoren Übung erstellt

---
+++ src/main/java/demian/week01/day04/LogischeOperatoren.java
@@ -0,0 +1,24 @@
+package demian.week01.day04;
+
+public class LogischeOperatoren {
+    public static void main(String[] args) {
+        //ARBEITEN MIT LOGISCHEN OPERATOREN
+
+        /*ÜBUNG 1:
+        Größer als 3 und eine gerade Zahl oder 11
+         */
+
+/*
+        Random random = new Random();
+        //Random number between 100 and 100
+        int randomNumber = random.nextInt(-100, 101);
+
+        System.out.println("Uebung 0");
+        System.out.println("Generierte Zufallszahl:"+random);
+        if (random > 3 && (random % 2 == 0) || random == 0){
+            System.out.println("Zahl ist Größer als 3 und eine gerade Zahl oder 11");
+        } else {
+            System.out.println("Zahl ist NICHT Größer als 3 und eine gerade Zahl oder 11");}
+*/
+    }
+}
