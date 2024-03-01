63ecf8a766e174e0fbb37bcc02cd14582d60017e
Irem <aksoy.irem5757@gmail.com>
2024-03-01 11:14:58 +0100

    LogischeOperatoren 0-2 Triangle Top Right

---
+++ src/main/java/irem/LogischeOperatoren0.java
@@ -0,0 +1,28 @@
+package irem;
+
+public class LogischeOperatoren0 {
+    public static void main(String[] args) {
+        System.out.println(isBiggerThanTenAndEvenOrBiggerThanZero(54));
+        System.out.println(isBiggerThanTenAndEvenOrBiggerThanZero2(53));
+
+    }
+
+    public static boolean isBiggerThanTenAndEvenOrBiggerThanZero(int value) {
+        return value > 10 && (value % 2 == 0);
+    }
+
+    public static boolean isBiggerThanTenAndEvenOrBiggerThanZero2(int value) {
+        if (value == 0) {
+            return true;
+        } else if (value > 10) {
+            if (value % 2 == 0) {
+                return true;
+            } return false;
+        } else {
+            return false;
+        }
+    }
+
+
+}
+
