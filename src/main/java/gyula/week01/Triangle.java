1facdd3f8603422b1860d88e251e169c1d70b6ea
Gyula Horvath <gylhrvth@gmail.com>
2024-03-01 10:24:43 +0100

    Rombus Example

--- src/main/java/gyula/week01/Triangle.java
+++ src/main/java/gyula/week01/Triangle.java
@@ -2,13 +2,31 @@ package gyula.week01;
 
 public class Triangle {
     public static void main(String[] args) {
-        triangle("A ", 15);
+        //triangle("A ", 15);
+        rombus("A ", 5);
     }
 
     public static void triangle(String text, int size){
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < 2 * size -1; col++) {
                 if (row + col == size -1 || col == row + size - 1 || row == size -1) {
+                    System.out.print(text);
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void rombus(String text, int size){
+        for (int row = 0; row < 2 * size - 1; row++) {
+            for (int col = 0; col < 2 * size - 1; col++) {
+                if (    row + col == size -1  ||
+                        col == row + size -1  ||
+                        col + size - 1 == row ||
+                        row + col == 3 * size - 3 ) {
                     System.out.print(text);
                 } else {
                     System.out.print(". ");
