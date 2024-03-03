24633c06fe1d1114eec2c860749962e617136384
schmid <schmid100@hotmail.com>
2024-03-03 18:31:05 +0100

    - EmptySquare-String-Rückgabe mit Stringbuilder - Stringbuilder in Methode in Methode verwenden - DecimalFormat und RandomNumber ausprobieren

--- src/main/java/christian/week01/day05/DecimalFormatTryOut.java
+++ src/main/java/christian/week01/day05/DecimalFormatTryOut.java
@@ -1,15 +1,20 @@
 package christian.week01.day05;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class DecimalFormatTryOut {
     public static void main(String[] args) {
-
+DezimalFormatTryOutMethod();
     }
-    public static void ZeroPointOneSteps(){
-
+    public static void DezimalFormatTryOutMethod(){
+        DecimalFormat decimalFormat = new DecimalFormat("###.#####");
+        double d = 25312352.84701;
+        System.out.println(decimalFormat.format(d));
+        long l = 1051;
+        System.out.println(decimalFormat.format(l/Math.pow(10,2)));
     }
     public static void RandomNumberZeroToHundred(){
-        Random random = new Random()
+        Random random = new Random();
     }
 }
