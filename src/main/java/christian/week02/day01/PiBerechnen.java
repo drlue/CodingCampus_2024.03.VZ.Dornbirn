16126b7ab585ffe711587de801b77796faba05ac
Christian Schmid <schmid100@hotmail.com>
2024-03-04 09:52:12 +0100

    -Week 2 Start piBerechnen usw.

--- src/main/java/christian/week02/day01/PiBerechnen.java
+++ src/main/java/christian/week02/day01/PiBerechnen.java
@@ -2,18 +2,19 @@ package christian.week02.day01;
 
 public class PiBerechnen {
     public static void main(String[] args) {
+
         System.out.println(calculatePiWithLeibnizAndPrecision(0.0001));
     }
+
     public static double calculatePiWithLeibnizAndPrecision(double precision) {
         double piMax = 4;
         double piMin = 0;
         int nenner = 1;
-        int i = 1;
         while (piMax - piMin >= precision) {
-            piMax = piMin+4.0/nenner;
-            nenner+=2;
-            piMin = piMax-4.0/nenner;
-            nenner+=2;
+            piMax = piMin + 4.0 / nenner;
+            nenner += 2;
+            piMin = piMax - 4.0 / nenner;
+            nenner += 2;
         }
         return piMax;
     }
