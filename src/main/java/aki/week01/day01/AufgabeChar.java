a4a3b9209f2da574e3a527510ad5d2264fd82dc0
4kra <aalihadhiev@gmail.com>
2024-03-06 13:46:17 +0100

    Input Uebungen

--- src/main/java/aki/week01/day01/AufgabeChar.java
+++ src/main/java/aki/week01/day01/AufgabeChar.java
@@ -30,25 +30,27 @@ public class AufgabeChar {
         }
     }
 
-    public static void printTriangleBottomLeft(String zeichen, int number) {
+    public static int printTriangleBottomLeft(String zeichen, int number) {
         for (int i = 0; i <= number; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(zeichen);
             }
             System.out.println();
         }
+        return number;
     }
 
-    public static void printTriangleTopLeft(String zeichen, int number) {
+    public static int printTriangleTopLeft(String zeichen, int number) {
         for (int i = number; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(zeichen);
             }
             System.out.println();
         }
+        return number;
     }
 
-    public static void printTriangleTopRight(String zeichen, int number) {
+    public static int printTriangleTopRight(String zeichen, int number) {
         for (int i = 0; i < number; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
@@ -58,9 +60,10 @@ public class AufgabeChar {
             }
             System.out.println();
         }
+        return number;
     }
 
-    public static void printTriangleBottomRight(String zeichen, int number) {
+    public static int printTriangleBottomRight(String zeichen, int number) {
         for (int i = number; i >= 0; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
@@ -70,9 +73,10 @@ public class AufgabeChar {
             }
             System.out.println();
         }
+        return number;
     }
 
-    public static void printEmptySquare(String zeichen, int number) {
+    public static int printEmptySquare(String zeichen, int number) {
         for (int i = 0; i < number; i++) {
             for (int j = 0; j < number; j++) {
                 if (i == 0 || i == number - 1 || j == 0 || j == number - 1) {
@@ -83,9 +87,10 @@ public class AufgabeChar {
             }
             System.out.println();
         }
+        return number;
     }
 
-    public static void printTriangle(String zeichen, int size) {
+    public static int printTriangle(String zeichen, int size) {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size * 2 - 1; col++) {
                 if (col == size - row - 1 || col == size + row - 1 || row == size - 1) {
@@ -96,12 +101,13 @@ public class AufgabeChar {
             }
             System.out.println();
         }
+        return size;
     }
 
     public static void printRhombus(String x, String leer, String dot, int size) {
         for (int row = 0; row < size * 2; row++) {
             for (int col = 0; col < size * 4; col++) {
-                if (col == size - row -1 || col == size + row -1) {
+                if (col == size - row - 1 || col == size + row - 1) {
                     System.out.print(x);
                 } else {
                     System.out.print(dot);
