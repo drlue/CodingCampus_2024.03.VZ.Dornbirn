8d62f217b3d59c47575a168d840822aad32fa125
Irem <aksoy.irem5757@gmail.com>
2024-03-06 15:26:31 +0100

    Menu Aufgabe

--- src/main/java/irem/week02/Menu.java
+++ src/main/java/irem/week02/Menu.java
@@ -1,14 +1,31 @@
 package irem.week02;
 
+import irem.week01.day01.PrintTriangleTopRight;
+
 public class Menu {
     public static void main(String[] args) {
-        System.out.println("Wilkommen bei meinen Aufgaben \n was möchten sie zeichnen  ");
+        System.out.println("Wilkommen bei meinen Aufgaben \n was möchten sie zeichnen \n " +
 
+                "1 TriangleTopRight" +
+                " 2 TriangleBottomRight");
 
+        int wahl = UserInput.getIntFromConsole("Bitte wählen sie nun", 1, 2);
+        if(wahl==1){
+        int size = UserInput.getIntFromConsole("Wie groß soll es sein?", 1, 25);
+        String text = UserInput.getTextFromConsole("Welches zeichen soll verwendet werden: ");
+        PrintTriangleTopRight.printTriangleTopRight(text, size);
 
+    }else{
+        int size3 = UserInput.getIntFromConsole("Wie groß soll es sein?", 1, 25);
+        String text3 = UserInput.getTextFromConsole("Welches zeichen soll verwendet werden: ");
+        PrintTriangleTopRight.printTriangleBottomRight(text3, size3);}
 
+        String wahl2 = UserInput.getTextFromConsole("Möchtesn sie noch etwas Zeichnen \n Ja || Nein");
+        if (wahl2.equals("Ja")) {
 
+        } else {
 
+        }
     }
 
 }
