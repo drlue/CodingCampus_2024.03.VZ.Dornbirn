689e0adb2be901541ecbd4ffee445802a68cb624
Demian <demian.fitz@gmail.com>
2024-03-08 12:02:00 +0100

    - Aufgabe abgeschlossen: Number Array

---
+++ src/main/java/demian/week02/day05/AufgabeNumberArray.java
@@ -0,0 +1,34 @@
+package demian.week02.day05;
+
+import demian.week02.day02.ScannerNachbau;
+
+import java.util.Arrays;
+
+public class AufgabeNumberArray {
+
+
+    public static void main(String[] args) {
+
+        //AUFGABE:
+        // Erstelle ein int[] mit der Dimension von Benutzerangabe und befülle mit 1, 2, 3, usw.
+
+        System.out.println("Array erstellen und mit 1,2,3... befüllen");
+        int userinput = ScannerNachbau.consoleINTEGERinput("Geben Sie bitte die gewünschte Größe des Arrays an ==>", 0, 888);
+        int[] userSIZEDarray = new int[userinput];
+        for (int i = 0; i < userinput; i++) {
+            userSIZEDarray[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(userSIZEDarray));
+
+        // Erstelle ein zweites int[] und befülle mit Zahlen entsprechend der Größe ..., 3, 2, 1.
+
+        System.out.println("Array erstellen und mit 10,9,8,...,1 befüllen");
+        userinput = ScannerNachbau.consoleINTEGERinput("Geben Sie bitte die gewünschte Größe des Arrays an ==>", 0, 888);
+        int[] userSIZEDarray2 = new int[userinput];
+        for (int arrayELEMENT = 0; arrayELEMENT < userinput; arrayELEMENT++) {
+            userSIZEDarray2[arrayELEMENT] = userinput-arrayELEMENT;
+        }
+        System.out.println(Arrays.toString(userSIZEDarray2));
+
+    }
+}
