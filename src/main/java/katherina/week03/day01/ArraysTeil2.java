56ab4726ed007a9878f3595f09adaf4852289695
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-03-11 12:20:52 +0100

    - did the copy of the array task with help and wrote a documentation to practice the most un-confusing phrasing of this kind of stuff

--- src/main/java/katherina/week03/day01/ArraysTeil2.java
+++ src/main/java/katherina/week03/day01/ArraysTeil2.java
@@ -3,8 +3,6 @@ package katherina.week03.day01;
 import java.util.Arrays;
 import java.util.Random;
 
-import static java.lang.System.in;
-
 public class ArraysTeil2 {
 
     static Random random = new Random();
@@ -12,9 +10,9 @@ public class ArraysTeil2 {
     public static void main(String[] args) {
         int randomNumber = random.nextInt(101);
 
-        int[] zufallswerte = new int[15];
-        //Ich habe meine Schublade mit "Zufallswerte" benannt, sind ja mehrere. Und ich bestimme, dass sie 15 Werte enthält.
-
+        int[] zufallswerte = new int[101];
+        //Ich habe meine Schublade mit "Zufallswerte" benannt, sind ja mehrere. Und ich bestimme, dass sie 100 Werte enthält.
+        zufallswerte[randomNumber] = randomNumber;
         System.out.println(Arrays.toString(zufallswerte));
 
 
