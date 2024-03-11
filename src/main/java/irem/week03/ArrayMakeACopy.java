37084c65027b4d5e33def6f880cea2eef676cea9
Irem <aksoy.irem5757@gmail.com>
2024-03-11 15:44:21 +0100

    ArrayMakeACopy ArrayRandomNumber RandomNumberArrayCrazyRange ArraySum

--- src/main/java/irem/week03/ArrayMakeACopy.java
+++ src/main/java/irem/week03/ArrayMakeACopy.java
@@ -16,10 +16,9 @@ public class ArrayMakeACopy {
 
 
     public static int[] makeACopy(int[] original) {
-        int[]copy=new int[original.length];
+        int[] copy = new int[original.length];
         for (int i = 0; i < original.length; i++) {
-            copy[i]=original[i];
-            System.out.println(copy[i]);
+            copy[i] = original[i];
         }
 
         return copy;
