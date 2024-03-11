da4dd28f20203faff5712fd7eaff3cacc22f3915
Demian <demian.fitz@gmail.com>
2024-03-11 14:55:17 +0100

    -Aufgabe geloest: RandomNumberArraySumme

--- src/main/java/demian/week03/day01/AufgabeRandomNumberArraySumme.java
+++ src/main/java/demian/week03/day01/AufgabeRandomNumberArraySumme.java
@@ -12,12 +12,26 @@ public class AufgabeRandomNumberArraySumme {
 
         Random random = new Random();
 
-        int[] randomNumberArray = new int[5];
+        int[] randomNumberArray = new int[3];
 
         for (int element = 0; element < randomNumberArray.length; element++) {
             randomNumberArray[element] = random.nextInt(0, 101);
         }
         System.out.println("\nAUFGABE: Erstelle ein int[] und befülle es mit Zufallszahlen von 1 bis 100.");
         System.out.println("\nRandom Number Array: " + Arrays.toString(randomNumberArray));
+        printSUMofArrayElements(randomNumberArray);
+
+
     }
+
+    public static void printSUMofArrayElements (int[] integerARRAY){
+        int sum = 0;
+        int num = 0;
+        for (int element = 0; element < integerARRAY.length; element++) {
+            num = integerARRAY[element];
+            sum = sum + num;
+        }
+        System.out.println("\nSumme aller Elemente des Arrays: " + sum);
+    }
+
 }
