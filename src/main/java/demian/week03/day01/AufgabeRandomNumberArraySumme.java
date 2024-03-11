e13db8a26933bb554972ec1a3be1034d12ac2a6e
Demian <demian.fitz@gmail.com>
2024-03-11 14:49:57 +0100

    -Class umbenannt: RandomNumberArrayZaehlen

--- src/main/java/demian/week03/day01/AufgabeRandomNumberArraySumme.java
+++ src/main/java/demian/week03/day01/AufgabeRandomNumberArraySumme.java
@@ -1,4 +1,23 @@
 package demian.week03.day01;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class AufgabeRandomNumberArraySumme {
+
+    public static void main(String[] args) {
+
+        //AUFGABE: Erstelle ein int[] und befülle es mit Zufallszahlen von 1 bis 100.
+        //Erstelle eine Funktion, die die Summe der Werte zurückgibt.
+
+        Random random = new Random();
+
+        int[] randomNumberArray = new int[5];
+
+        for (int element = 0; element < randomNumberArray.length; element++) {
+            randomNumberArray[element] = random.nextInt(0, 101);
+        }
+        System.out.println("\nAUFGABE: Erstelle ein int[] und befülle es mit Zufallszahlen von 1 bis 100.");
+        System.out.println("\nRandom Number Array: " + Arrays.toString(randomNumberArray));
+    }
 }
--- src/main/java/demian/week03/day01/AufgabeRandomNumberArrayZaehlen.java
+++ src/main/java/demian/week03/day01/AufgabeRandomNumberArrayZaehlen.java
@@ -3,7 +3,7 @@ package demian.week03.day01;
 import java.util.Arrays;
 import java.util.Random;
 
-public class AufgabeRandomNumberArrayZählen {
+public class AufgabeRandomNumberArrayZaehlen {
     public static void main(String[] args) {
 
         //AUFGABE: Erstelle ein int[] und befülle es mit Zufallszahlen von 1 bis 100.
