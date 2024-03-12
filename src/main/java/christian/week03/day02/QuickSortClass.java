31e019eba465061189a96a340ed62af64cd26b3d
schmid <schmid100@hotmail.com>
2024-03-12 17:49:42 +0100

    -Fehler in quickSort gefunden

--- src/main/java/christian/week03/day02/QuickSortClass.java
+++ src/main/java/christian/week03/day02/QuickSortClass.java
@@ -10,7 +10,7 @@ public class QuickSortClass {
     public static void main(String[] args) {
         int[] array = createRandomNumberArray(9, 0, 101);
         System.out.println(Arrays.toString(array));
-        quickSort(array,0,array.length-1);
+        quickSort(array, 0, array.length - 1);
 
     }
 
@@ -24,12 +24,13 @@ public class QuickSortClass {
 
     public static void quickSort(int[] arr, int l, int r) {
         if (l >= r) {
-            return; }
+            return;
+        }
 
-            int q = random.nextInt(l, r+1);
-            int m = partition(arr, l, r, q);
-            quickSort(arr, l, m-1);
-            quickSort(arr, m+1, r);
+        int q = random.nextInt(l, r + 1);
+        int m = partition(arr, l, r, q);
+        quickSort(arr, l, m - 1);
+        quickSort(arr, m + 1, r);
         System.out.println(Arrays.toString(arr));
 
     }
@@ -40,7 +41,7 @@ public class QuickSortClass {
         arr[r] = p;
         int i = l;
 
-        for (int j = l; j < r ; j++) {
+        for (int j = l; j < r; j++) {
             if (arr[j] <= p) {
                 int temp2 = arr[i];
                 arr[i] = arr[j];
