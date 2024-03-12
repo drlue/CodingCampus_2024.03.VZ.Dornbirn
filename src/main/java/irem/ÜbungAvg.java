be5dc328571d34c6c6be941aeaafeb0a81f6e9a0
Irem <aksoy.irem5757@gmail.com>
2024-03-12 14:00:21 +0100

    ÜbungAvg

---
+++ src/main/java/irem/ÜbungAvg.java
@@ -0,0 +1,20 @@
+package irem;
+
+public class ÜbungAvg {
+    public static void main(String[] args) {
+        int[] test = {1, 2, 3, 4};
+        double avgeResult = avarage(test);
+        System.out.println("AVG:" + avgeResult);
+    }
+
+    public static double avarage(int[] arr) {
+        int summe = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summe += arr[i];
+        }
+
+        return (double)summe / arr.length;
+    }
+}
+
+//auf die nennung der namen achten!
