02ed7c9b5af43c4425c6033c3a5c47332a112e3b
Gyula Horvath <gylhrvth@gmail.com>
2024-03-13 11:52:18 +0100

    Sort Example

---
+++ src/main/java/gyula/week03/BubbleSortExample.java
@@ -0,0 +1,26 @@
+package gyula.week03;
+
+import gyula.week02.ArrayExample;
+
+import java.util.Arrays;
+
+public class BubbleSortExample {
+    public static void main(String[] args) {
+        int[] arr = PrintArray.createRandomArray(20);
+        System.out.println(Arrays.toString(arr));
+        sortBubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sortBubble(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
