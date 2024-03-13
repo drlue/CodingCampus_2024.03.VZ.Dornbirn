690e6ca29ed7ff1642792e5e81de8a488ac3ac4a
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-03-13 13:50:25 +0100

    - finished min/max/avg - now for real!

--- src/main/java/katherina/week03/day02/Bubblesort.java
+++ src/main/java/katherina/week03/day02/Bubblesort.java
@@ -1,9 +1,9 @@
 package katherina.week03.day02;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Arrays;
 import java.util.Random;
-
-import static java.util.Arrays.sort;
 
 public class Bubblesort {
     static Random random = new Random();
@@ -11,10 +11,17 @@ public class Bubblesort {
     public static void main(String[] args) {
         int[] zufallswerte = createRandomArray(10);
         printWithForI(zufallswerte);
+        System.out.println(">>Dies ist unsortiert! Hiiiiilfe! Sortier das!<<");
+        System.out.println();
         bubblesortNachHoch(zufallswerte);
+        System.out.println(">>So?<<");
+        printWithForI(zufallswerte);
+        System.out.println();
         bubblesortNachUnten(zufallswerte);
-        System.out.println(Arrays.toString(zufallswerte));
-
+        System.out.println(">>Oder lieber so?<<");
+        printWithForI(zufallswerte);
+        System.out.println();
+        System.out.println("Super, passt! Danke schön!");
     }
 
     public static void printWithForI(int[] arr) {
@@ -22,12 +29,11 @@ public class Bubblesort {
             for (int index = 0; index < arr.length - 1; ++index) {
                 System.out.print(arr[index] + ", ");
             }
-            System.out.print(arr[arr.length - 1] + " ");
-            System.out.println();
+            System.out.println(arr[arr.length - 1]);
         }
-        System.out.println(">>Dies ist unsortiert! Hiiiiilfe! Sortier das!<<");
     }
 
+    //Variable 'jane' heißt so aus Accessibility-Gründen. Ich kann sonst nicht gut sehen, ob etwas i oder j ist.
     public static void bubblesortNachHoch(int[] array) {
         for (int index = 0; index < array.length; index++) {
             for (int jane = 0; jane < array.length - index - 1; jane++) {
@@ -41,15 +47,23 @@ public class Bubblesort {
     }
 
     public static void bubblesortNachUnten(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i -1];
+            array[array.length - i -1] = temp;
+        }
+
+/*
         for (int index = 0; index < array.length; index++) {
             for (int jane = 0; jane < array.length - index - 1; jane++) {
-                if (array[jane] > array[jane + 1]) {
+                if (array[jane] < array[jane + 1]) {
                     int temp = array[jane];
                     array[jane] = array[jane + 1];
                     array[jane + 1] = temp;
                 }
             }
         }
+ */
     }
 
     public static int[] createRandomArray(int size) {
@@ -59,4 +73,6 @@ public class Bubblesort {
         }
         return result;
     }
+
+
 }
