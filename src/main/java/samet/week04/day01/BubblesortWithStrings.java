e4cd290a2091a07378129cfdd78937759ad17210
samy0307 <samet.sevimli07@gmail.com>
2024-03-18 19:01:28 +0100

    week04 StringUebungen

---
+++ src/main/java/samet/week04/day01/BubblesortWithStrings.java
@@ -0,0 +1,22 @@
+package samet.week04.day01;
+
+import java.util.Scanner;
+
+public class BubblesortWithStrings {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String names[] = new String[12];
+
+        System.out.println("Enter 12 names");
+
+
+        for (int i = 0; i < names.length; i++) {
+        }
+//        names[i] = sc.nextLine();
+    }
+
+
+}
