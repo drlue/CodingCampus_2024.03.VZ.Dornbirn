f6a9bc2ccb792a5fecd20a4c1300e69291896197
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-03-20 14:58:18 +0100

    - I slightly overdid my chaos magic for the String Randomize task! Whoops! Take a look at what happens, especially if you test my code with longer words. (If you review the code you will see why this happens. I will try to solve that - but that's a task for tomorrow-me.)

--- src/main/java/katherina/week04/day03/DieErsteTabelle.java
+++ src/main/java/katherina/week04/day03/DieErsteTabelle.java
@@ -1,18 +1,34 @@
 package katherina.week04.day03;
 
 //Erstelle einen schönen tabellarischen Ausgabe aus den folgenden Arrays:
+
 //
-//String[] firstName = {"Alfonso", "Beatrix-Eleonor", "Cecil", "Daniel", "Elmar"};
-//String[] lastName = {"Klein", "Kinderdorfer", "Al Elmenar", "Schmidt", "Simma"};
-//int[] age = {40, 78, 5, 18, 81};
-//String[] place = {"Wien", "Schwarzach", "Wiener Neudorf", "Sankt Pölten", "Sankt Pölten"};
-//float[] distanceFromCapital = {0f, 654.4f, 12.457634366f, 120.0f, 119.9999f};
 // Denke daran, dass eine Tabelle eine Kopfzeile und Trennzeichen haben kann.
 
 
+import java.util.Arrays;
 
 public class DieErsteTabelle {
     public static void main(String[] args) {
+        System.out.printf("Vorname %-6s ","");
+        String[] firstName = {"Alfonso", "Beatrix-Eleonor", "Cecil       ", "Daniel   ", "Elmar"};
+        String message = String.join(" | ", firstName);
+        System.out.println(Arrays.toString(new String[]{message}));
+        System.out.printf("Nachname %-5s ","");
+        String[] lastName = {"Klein", "Kinderdorfer ", "Al Elmenar", "Schmidt", "Simma"};
+        String message2 = String.join("   | ", lastName);
+        System.out.println(Arrays.toString(new String[]{message2}));
+        System.out.printf("Alter%-9s ","");
+        int[] age = {40, 78, 5, 18, 81};
+        String message3 = String.join(" | ", Arrays.toString(age));
+        System.out.println(Arrays.toString(new String[]{message3}));
+        System.out.printf("Wohnort%-7s ","");
+        String[] place = {"Wien ", "Schwarzach   ", "Wiener Neudorf", "Sankt Pölten", "Sankt Pölten"};
+        String message4 = String.join("   | ", place);
+        System.out.println(Arrays.toString(new String[]{message4}));
+        System.out.printf("Entfernung zur \nHauptstadt %-3s ","");
+        float[] distanceFromCapital = {0f, 654.4f, 12.457634366f, 120.0f, 119.9999f};
+        System.out.println(Arrays.toString(distanceFromCapital));
 
     }
 }
