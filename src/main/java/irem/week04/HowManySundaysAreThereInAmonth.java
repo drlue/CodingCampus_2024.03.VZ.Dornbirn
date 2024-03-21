919acf274a5894b5d7cdd024d176a7a723b6d4bb
Irem <aksoy.irem5757@gmail.com>
2024-03-21 15:34:05 +0100

    HowManySundaysAreThereInAmonth

---
+++ src/main/java/irem/week04/HowManySundaysAreThereInAmonth.java
@@ -0,0 +1,12 @@
+package irem.week04;
+
+import java.util.Scanner;
+
+public class HowManySundaysAreThereInAmonth {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+    }
+}
