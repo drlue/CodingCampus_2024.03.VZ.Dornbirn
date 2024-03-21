17c79dcb210ca328400cba615e9a9dbee6c7c8cd
samy0307 <samet.sevimli07@gmail.com>
2024-03-21 08:49:13 +0100

    week04 day04

---
+++ src/main/java/samet/week04/Tabelle.java
@@ -0,0 +1,13 @@
+package samet.week04;
+
+public class Tabelle {
+
+    public static void main(String[] args) {
+
+        String[] firstName = {"Alfonso", "Beatrix-Elenor", "Cecil", "Daniel, Elmar"};
+        String[] lastName = {"Klein", "Kinderdorfer", "Al Elmenar", "Schmidt, Simma"};
+        int[] age = {40, 78, 5, 18, 81};
+        String[] place = {"Wien","Schwarzach", "Wiener Neudorf", "Sankt Pölten", "Sankt Pölten"};
+        float[] distanceFromCapital = {0f, 654.4f, 12.45763466f, 120.0f, 119.9999f};
+    }
+}
