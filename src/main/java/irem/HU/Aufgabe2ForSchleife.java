30fcfa52fe7a5a2ea9401ee4cba1239a95b28551
Irem <aksoy.irem5757@gmail.com>
2024-03-21 20:39:08 +0100

    Aufgabe2ForSchleife AufgabeMethoden1

--- src/main/java/irem/HU/Aufgabe2ForSchleife.java
+++ src/main/java/irem/HU/Aufgabe2ForSchleife.java
@@ -1,8 +1,8 @@
 package irem.HU;
 
 public class Aufgabe2ForSchleife {
-    {
-    }
+
+
         public static void main(String[] args) {
             for (int i = -30; i <= 30; i++) {
                 if (i % 3 == 0) {
