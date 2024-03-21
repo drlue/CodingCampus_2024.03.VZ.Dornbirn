917ddd1267df6d767dbb54255b4c397f6bfe7eda
Irem <aksoy.irem5757@gmail.com>
2024-03-21 20:14:37 +0100

    Aufgabe2ForSchleife

---
+++ src/main/java/irem/HU/Aufgabe2ForSchleife.java
@@ -0,0 +1,14 @@
+package irem.HU;
+
+public class Aufgabe2ForSchleife {
+    public class DivisibleByThree {
+    }
+        public static void main(String[] args) {
+            for (int i = -30; i <= 30; i++) {
+                if (i % 3 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+
