63a30adbdea10035e75181d31f70fe652f0ac912
LZE <lukas.zech@gmx.at>
2024-03-22 08:59:50 +0100

    - added one line to my passive-aggressive error code. - did the next sunday script

--- src/main/java/katherina/week04/day04/NextSunday.java
+++ src/main/java/katherina/week04/day04/NextSunday.java
@@ -8,16 +8,26 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.DAY_OF_WEEK;
+
 public class NextSunday {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
         Date today = new Date();
 
-        DateFormat dataformat = new SimpleDateFormat("EEEEEEEEE");
+        DateFormat dataformat = new SimpleDateFormat("EEEE");
         DateFormat dataformat2 = new SimpleDateFormat("dd.MM.yyyy");
-        System.out.println("Der heutige Wochentag ist ein: " + dataformat.format(calendar.get(Calendar.DAY_OF_WEEK)) + " am " + dataformat2.format(today) + ".");
+
+        System.out.println("Der heutige Wochentag ist ein " + dataformat.format(today) + ", am " + dataformat2.format(today) + ".");
         System.out.println("Wann ist der nächste Sonntag?");
-        calendar.add(Calendar.DAY_OF_WEEK, 1);
+        calendar.setTime(today);
+        int weekdaynumber = calendar.get(DAY_OF_WEEK);
+        int sundayCountDown = Calendar.SUNDAY + 7 - weekdaynumber;
+        calendar.add(Calendar.DAY_OF_YEAR, sundayCountDown);
+        Date sunday = calendar.getTime();
+        System.out.println("Der nächste Sonntag ist in " + sundayCountDown + " Tagen!");
+        System.out.print("Und zwar am " + dataformat2.format(sunday));
 
 
     }
--- src/main/java/lukas/week04/NextSunday.java
+++ src/main/java/lukas/week04/NextSunday.java
@@ -1,24 +1,32 @@
 package lukas.week04;
 
+import lukas.Helper;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class NextSunday {
 
     public static void main(String[] args) {
-
-        Date now = new Date();
-
-
-
+        Date myDate = Helper.readDateFromConsole("Gib ein Datum ein >>>");
+        Date nextSun = getDateOfNextSunday(myDate);
+        DateFormat df1 = new SimpleDateFormat("EEEE");
+        DateFormat df2 = new SimpleDateFormat("dd.MM.yyyy");
+        System.out.printf("%-20s %-10s %-10s\n", "gew. Datum:", df1.format(myDate), df2.format(myDate));
+        System.out.printf("%-20s %-10s %-10s\n", "nächster Sonntag:", df1.format(nextSun), df2.format(nextSun));
     }
 
     public static Date getDateOfNextSunday(Date now) {
-
         Calendar cal = Calendar.getInstance();
-
-
-return null;
+        cal.setTime(now);
+        int dayNr = cal.get(Calendar.DAY_OF_WEEK);
+        int daysTillSunday = Calendar.SUNDAY + 7 - dayNr;
+        cal.add(Calendar.DAY_OF_YEAR, daysTillSunday);
+        return cal.getTime();
     }
 
 }
