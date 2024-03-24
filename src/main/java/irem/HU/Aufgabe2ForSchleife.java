b5bf6ed036bb81350c7d8d26f33cdf7ea531f396
Irem <aksoy.irem5757@gmail.com>
2024-03-24 16:40:24 +0100

    Aufgabe2ForSchleife

--- src/main/java/irem/HU/Aufgabe2ForSchleife.java
+++ src/main/java/irem/HU/Aufgabe2ForSchleife.java
@@ -2,13 +2,20 @@ package irem.HU;
 
 public class Aufgabe2ForSchleife {
 
+    public static void main(String[] args) {
+        printRowStipple("X", 3);
 
-        public static void main(String[] args) {
-            for (int i = -30; i <= 30; i++) {
-                if (i % 3 == 0) {
-                    System.out.println(i);
+        }
+        public static void printRowStipple (String text,int repetitions){
+            for (int i = 0; i < repetitions; i++) {
+                System.out.println(text);
+                if (i < repetitions - 1) {
+                    System.out.println();
                 }
             }
         }
     }
+
+
+
 
