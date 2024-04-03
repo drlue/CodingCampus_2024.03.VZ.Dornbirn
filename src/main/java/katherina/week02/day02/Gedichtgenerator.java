34b63662a726f163d5da338fc65a11c935920ef1
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-04-03 14:24:42 +0200

    Finished the simple file writing exercise. Finished file writing table. Decided that I want to incorporate this into my pantun generator so now technically, the file is saved. I haven't changed the explanation, though, since I would want to implement the possibility to create a new file in a directory chosen by the user and I'm not sure how to do that yet.

--- src/main/java/katherina/week02/day02/Gedichtgenerator.java
+++ src/main/java/katherina/week02/day02/Gedichtgenerator.java
@@ -1,5 +1,8 @@
 package katherina.week02.day02;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Gedichtgenerator {
@@ -13,6 +16,7 @@ public class Gedichtgenerator {
                         "Bitte vergiss nicht, dein Gedicht aus der Konsole zu kopieren, sobald es fertig ist. Sonst ist es hinterher weg! Auf nimmerwiedersehen! Futsch!");
         System.out.println();
         System.out.println();
+        String text9 = getTextFromConsole("Bitte gib die Überschrift deines Gedichtes ein:");
         String text = getTextFromConsole("Bitte gib die erste Zeile der ersten Strophe ein:");
         String text2 = getTextFromConsole("Bitte gib die zweite Zeile der ersten Strophe ein:");
         String text3 = getTextFromConsole("Bitte gib die dritte Zeile der ersten Strophe ein, sie sollte sich auf Zeile 1 reimen:");
@@ -23,6 +27,8 @@ public class Gedichtgenerator {
         String text8 = getTextFromConsole("Bitte gib die vierte Zeile der dritten Strophe ein, sie sollte sich auf Zeile 2 reimen:");
 
 
+        System.out.println(text9); //Überschrift
+        System.out.println();
         System.out.println(text); //a , 1
         System.out.println(text2);//b , 2
         System.out.println(text3);//a , 3
@@ -42,6 +48,41 @@ public class Gedichtgenerator {
         System.out.println(text3);//a , 3
         System.out.println(text8);//d , 8
         System.out.println(text); //a , 1
+        System.out.println();
+
+
+
+        try {
+            FileOutputStream file = new FileOutputStream("C:\\Users\\DCV\\IdeaProjects\\CodingCampus_2024.03.VZ.Dornbirn\\src\\main\\java\\katherina\\week02\\day02\\GenerierteGedichte\\pantunfile.txt", true);
+            PrintStream printedStream = new PrintStream(file);
+            printedStream.println(text9);
+            printedStream.println();
+            printedStream.println(text);
+            printedStream.println(text2);
+            printedStream.println(text3);
+            printedStream.println(text4);
+            printedStream.println();
+            printedStream.println(text2);
+            printedStream.println(text5);
+            printedStream.println(text4);
+            printedStream.println(text6);
+            printedStream.println();
+            printedStream.println(text5);
+            printedStream.println(text7);
+            printedStream.println(text6);
+            printedStream.println(text8);
+            printedStream.println();
+            printedStream.println(text7);
+            printedStream.println(text3);
+            printedStream.println(text8);
+            printedStream.println(text);
+            printedStream.println();
+
+
+        }
+        catch (FileNotFoundException fnfe){
+            System.out.println("Dein Text konnte nicht gespeichert werden. Die Datei wurde nicht erstellt.");
+        }
     }
 
     public static String getTextFromConsole(String message) {
@@ -50,4 +91,5 @@ public class Gedichtgenerator {
         String text = sc.nextLine();
         return text;
     }
+
 }
--- src/main/java/katherina/week05/day01/FileSchreiben.java
+++ src/main/java/katherina/week05/day01/FileSchreiben.java
@@ -1,13 +1,26 @@
 package katherina.week05.day01;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
+
 
 public class FileSchreiben {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Hi! Ich bewahre deine Texte auf, indem ich sie in einer Datei sichere.");
-BiggestData.getTextFromConsole("Bitte gib einen Text ein, den ich für dich aufbewahren soll.");
+String text = BiggestData.getTextFromConsole("Bitte gib einen Text ein, den ich für dich aufbewahren soll.");
 
+
+            try {
+                FileOutputStream file = new FileOutputStream("C:\\Users\\DCV\\IdeaProjects\\CodingCampus_2024.03.VZ.Dornbirn\\src\\main\\java\\katherina\\week05\\day01\\assetts\\tmp\\output.txt", true);
+                PrintStream printedStream = new PrintStream(file);
+                printedStream.println(text);
+            }
+            catch (FileNotFoundException fnfe){
+                System.out.println("Dein Text konnte nicht gespeichert werden. Die Datei wurde nicht erstellt.");
+            }
+        }
     }
-}
