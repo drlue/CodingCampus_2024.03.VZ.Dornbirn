dd5bcead45373906a0def7804c4be48209373cbb
Christian Schmid <schmid100@hotmail.com>
2024-04-08 10:52:11 +0200

    -CSV

--- src/main/java/christian/week05/CSV1.java
+++ src/main/java/christian/week05/CSV1.java
@@ -7,7 +7,7 @@ public class CSV1 {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\schmi\\IdeaProjects\\CodingCampus_2024.03.VZ.Dornbirn\\src\\main\\resources\\csv\\sales_100.csv");
         String csvString = CountLetters.readToString(file);
-        csvStringPrintFormated(csvString);
+        csvStringPrintFormated(csvString );
     }
 
     public static void csvStringPrintFormated(String csv) {
@@ -15,7 +15,7 @@ public class CSV1 {
         System.out.println(Arrays.toString(rowArray));
         for (int i = 0; i < rowArray.length; i++) {
             String [] colArray = rowArray[i].split(",");
-            System.out.println( colArray[i]);
+            System.out.println(colArray[i]);
         }
 
 
