d59fe2552a85468a59f973053eabaa02e1dc5891
Christian Schmid <schmid100@hotmail.com>
2024-04-09 15:49:15 +0200

    -FotoApparat mit File + Vector bis Aufgabe 2

--- src/main/java/christian/week06/collections/VectorTryOut.java
+++ src/main/java/christian/week06/collections/VectorTryOut.java
@@ -16,60 +16,65 @@ public class VectorTryOut {
         System.out.println(sortDescending(intVector));
         System.out.println(getMaximum(intVector));
         System.out.println(getMinimum(intVector));
+        System.out.println(deleteAllUnevenNumbers(intVector));
     }
 
-    public static Vector allEvenNumbers(Vector inputVector) {
+    public static Vector allEvenNumbers(Vector<Integer> inputVector) {
         Vector<Integer> outputVector = new Vector<Integer>();
-        if (!inputVector.isEmpty()) {
-            for (int i = 0; i < inputVector.size(); i++) {
-                if ((int) inputVector.get(i) % 2 == 0) {
-                    outputVector.add((int) inputVector.get(i));
-                }
+
+        for (int i = 0; i < inputVector.size(); i++) {
+            if (inputVector.get(i) % 2 == 0) {
+                outputVector.add(inputVector.get(i));
             }
         }
+
 
         return outputVector;
     }
 
-    public static int getMinimum(Vector inputVector) {
+    public static int getMinimum(Vector<Integer> inputVector) {
         int minimum = Integer.MAX_VALUE;
-        if (!inputVector.isEmpty()) {
-            for (int i = 0; i < inputVector.size(); i++) {
-                if ((int) inputVector.get(i) < minimum) {
-                    minimum = (int) inputVector.get(i);
-                }
+
+        for (int i = 0; i < inputVector.size(); i++) {
+            if (inputVector.get(i) < minimum) {
+                minimum = inputVector.get(i);
             }
         }
+
         return minimum;
     }
 
-    public static int getMaximum(Vector inputVector) {
+    public static int getMaximum(Vector<Integer> inputVector) {
         int maximum = Integer.MIN_VALUE;
-        if (!inputVector.isEmpty()) {
-            for (int i = 0; i < inputVector.size(); i++) {
-                if ((int) inputVector.get(i) > maximum) {
-                    maximum = (int) inputVector.get(i);
-                }
+
+        for (int i = 0; i < inputVector.size(); i++) {
+            if (inputVector.get(i) > maximum) {
+                maximum = inputVector.get(i);
             }
         }
+
         return maximum;
     }
 
-    public static Vector sortDescending(Vector inputVector) {
-        int temp = 0;
+    public static Vector sortDescending(Vector<Integer> inputVector) {
         boolean swapped = true;
         while (swapped) {
             swapped = false;
             for (int i = 1; i < inputVector.size(); i++) {
-
-                if ((int) inputVector.get(i) > (int) inputVector.get(i - 1)) {
-
-                    temp = (int) inputVector.get(i - 1);
-
-                    inputVector.set((i - 1), inputVector.get(i));
+                if (inputVector.get(i) > inputVector.get(i - 1)) {
+                    Integer temp = inputVector.set(i - 1, inputVector.get(i));
                     inputVector.set(i, temp);
                     swapped = true;
                 }
+            }
+        }
+        return inputVector;
+    }
+
+    public static Vector deleteAllUnevenNumbers(Vector<Integer> inputVector) {
+        for (int i = 0; i < inputVector.size(); i++) {
+            if ((float) inputVector.get(i) % 2 != 0.00f) {
+                inputVector.remove(i);
             }
         }
         return inputVector;
