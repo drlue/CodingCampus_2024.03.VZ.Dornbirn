0a764d2a24468a7e08a566b31f6f24ea2984d5b0
Gyula Horvath <gylhrvth@gmail.com>
2024-04-09 14:04:56 +0200

    Camera Obscura (v2.0)

--- src/main/java/gyula/week06/photo/Camera.java
+++ src/main/java/gyula/week06/photo/Camera.java
@@ -4,29 +4,38 @@ public class Camera {
     private String manufacture;
     private String model;
     private Objective objective;
+    private MemoryCard memory;
     private int megapixel;
 
     public Camera(String manufacture, String model, int megapixel){
         this.manufacture = manufacture;
         this.model = model;
         this.objective = null;
+        this.memory = null;
         this.megapixel = megapixel;
     }
 
     public void takePhoto(){
         if (objective == null) {
             System.out.println("Photo without objective is not possible");
+        } else if (memory == null) {
+            System.out.println("Photo without memorid card is not possible");
         } else {
-            System.out.println("Click!!!");
+            boolean saved = memory.savePHotos(megapixel);
+            if (saved){
+                System.out.println("Click!!!");
+            }
         }
     }
 
-    public Objective getObjective() {
-        return objective;
-    }
+
 
     public void setObjective(Objective objective) {
         this.objective = objective;
+    }
+
+    public void setMemory(MemoryCard memory) {
+        this.memory = memory;
     }
 
     @Override
--- src/main/java/gyula/week06/photo/PhotoApp.java
+++ src/main/java/gyula/week06/photo/PhotoApp.java
@@ -2,14 +2,19 @@ package gyula.week06.photo;
 
 public class PhotoApp {
     public static void main(String[] args) {
+        MemoryCard mc = new MemoryCard("Kingston", "4GB", 14);
         Objective obj = new Objective("Sony", "RLE-100", 18, 100);
         Camera sony = new Camera("Sony", "A-300", 12);
         sony.setObjective(obj);
+        sony.setMemory(mc);
         System.out.println(sony);
-        sony.takePhoto();
+
+        System.out.println(sony);
+        for (int i = 0; i < 20; i++) {
+            sony.takePhoto();
+        }
 
         sony.setObjective(null);
-        System.out.println(sony);
         sony.takePhoto();
     }
 }
