de1a9539e79a53383cb0756235e699db08edcf0a
Christian Schmid <schmid100@hotmail.com>
2024-04-10 20:54:57 +0200

    Ich komitte dann mal meinen Vektor ^^

--- src/main/java/christian/week06/fotoapparatPackage/test.java
+++ src/main/java/christian/week06/fotoapparatPackage/test.java
@@ -6,7 +6,7 @@ public class test {
     public static int photoCounter=0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Fotoapparat epson = new Fotoapparat("Megacam", "Fuchsmannenteprise", 1200, 600, sc);
+        Fotoapparat epson = new Fotoapparat("Megacam", "Fuchsmannenteprise", 1200, 600);
         Objektiv linse = new Objektiv(120, 220);
         Speicherkarte sp = new Speicherkarte(800,"Speicherkarte1");
         Objektiv megalinse = new Objektiv(200, 400);
