0c98b6dfe7899e0e333b9e43d5b83b027165a8c6
Demian <demian.fitz@gmail.com>
2024-04-11 14:22:38 +0200

    - Added gearshifter to bicycle

--- src/main/java/luki/week07/Fahrrad.java
+++ src/main/java/luki/week07/Fahrrad.java
@@ -9,4 +9,8 @@ public class Fahrrad extends Fahrzeug {
     public void fahre() {
         System.out.println("ICH fahre gemuetlich im Dorf mit 2 Reifen HAHAHAHHAHAHAH Calender ist outdated ");
     }
+
+    public void gangSchaltung(){
+        System.out.println("Ich schalte zwei Gänge rauf!");
+    }
 }
