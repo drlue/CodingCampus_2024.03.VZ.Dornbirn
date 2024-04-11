9ff8fe47f149004e3ee5c22c2e1d7b45258943a5
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-04-11 14:51:00 +0200

    Ich komitte dann mal meinen Vektor ^^

--- src/main/java/katherina/week06/day03/ZufallsVektor.java
+++ src/main/java/katherina/week06/day03/ZufallsVektor.java
@@ -96,7 +96,7 @@ public class ZufallsVektor {
 
     public static Vector ungeradeZahlenKillen(Vector<Integer> zufallsZahl) {
         for (int index = 0; index < zufallsZahl.size(); index++) {
-            if (zufallsZahl.get(index) % 2 != 0) {
+            while (index < zufallsZahl.size() && zufallsZahl.get(index) % 2 != 0) {
                 zufallsZahl.remove(index);
             }
         }
