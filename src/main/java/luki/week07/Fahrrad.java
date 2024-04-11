f5f7780e38c21990e937ea6ebb534b4cc8150163
Lukas Aichbauer <lukas.aichbauer@wolfvision.net>
2024-04-11 13:34:31 +0200

    - Added bicycle to class model.

---
+++ src/main/java/luki/week07/Fahrrad.java
@@ -0,0 +1,12 @@
+package luki.week07;
+
+public class Fahrrad extends Fahrzeug {
+    public Fahrrad(int geschwindigkeit) {
+        super(geschwindigkeit);
+    }
+
+    @Override
+    public void fahre() {
+        System.out.println("Ich fahre gemütlich im Dorf mit 2 Reifen");
+    }
+}
