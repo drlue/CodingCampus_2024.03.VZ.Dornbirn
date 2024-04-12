6f14490ae90f107099793d61a9f20aae2a6d3c08
Christian Schmid <schmid100@hotmail.com>
2024-04-12 11:45:13 +0200

    -zoo und permutation

--- src/main/java/christian/week06/tiergarten/Tier.java
+++ src/main/java/christian/week06/tiergarten/Tier.java
@@ -1,16 +1,37 @@
 package christian.week06.tiergarten;
 
+import christian.week06.tiergarten.hilfsKlassen.Futter;
+
 public class Tier {
     private String name;
     private String gattung;
+    private Futter futter;
+    private float futterMenge;
 
-    public Tier(String name, String gattung) {
+    public Tier(String name, String gattung, Futter futter, float futterMenge) {
         this.name = name;
         this.gattung = gattung;
-
+        this.futter = futter;
+        this.futterMenge = futterMenge;
     }
 
     public String toString() {
-        return "|---" + name + ", " + gattung;
+        return "\n    |---" + name + ", " + gattung;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGattung() {
+        return gattung;
+    }
+
+    public String getFutter() {
+        return futter.toString();
+    }
+
+    public float getFutterMenge() {
+        return futterMenge;
     }
 }
---
+++ src/main/java/christian/week06/tiergarten/hilfsKlassen/Futter.java
@@ -0,0 +1,32 @@
+package christian.week06.tiergarten.hilfsKlassen;
+
+public class Futter {
+
+    private String name;
+    private String einheit; // kg/g/liter
+    private float einheitsPreis; //Preis pro einheit
+
+    public Futter(String name, String einheit, float einheitsPreis) {
+        this.name = name;
+        this.einheit = einheit;
+        this.einheitsPreis = einheitsPreis;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEinheit() {
+        return einheit;
+    }
+
+    public float getEinheitsPreis() {
+        return einheitsPreis;
+    }
+    public void setEinheitsPreis(float neuerPreis){
+        this.einheitsPreis = neuerPreis;
+    }
+    public String toString(){
+        return "Futter: "+name+"in " + einheit+ "\n Preis pro Einheit in Euro: " + einheitsPreis;
+    }
+}
