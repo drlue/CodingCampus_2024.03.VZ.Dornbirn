c922a1ff73d3a26a51cf57d3a948fc08c9c35bbc
Christian Schmid <schmid100@hotmail.com>
2024-04-14 19:06:04 +0200

    -permutation not finished

--- src/main/java/christian/week06/permutationen/VektorPermutation.java
+++ src/main/java/christian/week06/permutationen/VektorPermutation.java
@@ -5,28 +5,25 @@ import java.util.Vector;
 
 public class VektorPermutation {
     public static void main(String[] args) {
-        Vector<String> vektor1 = new Vector<String>(Arrays.asList("A", "B", "C", "D"));
-        permutate(vektor1, 0, 0, "");
+        Vector<String> vektor1 = new Vector<String>();
+        Vector<String> vektor2 = new Vector<String>(Arrays.asList("A", "B", "C", "D"));
+        permutate(vektor1, vektor2);
     }
 
-    public static void permutate(Vector<String> vector, int index, int endcounter, String permutations) {
-
-//swap 1 Element
-        vector.set(index + 1, vector.set(index, vector.get(index + 1)));
-
-        permutations += "\n, " + vector.toString();
-        index++;
-        endcounter++;
-        if (index == vector.size() - 1) {
-            index = 0;
-
-        }
-        if (endcounter<christian.week01.day04.UebungenFaktoriell.faktoriell1(vector.size())){
-            permutate(vector, index, endcounter, permutations);
-        }
-        if (endcounter == christian.week01.day04.UebungenFaktoriell.faktoriell1(vector.size())){
-            System.out.println(permutations);
+    public static void permutate(Vector<String> vector, Vector<String> leftOver) {
+        String permutation = "";
+        for (int i = 0; i < leftOver.size(); i++) {
+            Vector<String> permVector = new Vector<String>();
+            permVector.add(leftOver.remove(i));
+            if (!leftOver.isEmpty()) {
+                permutate(permVector, leftOver);
+            } else {
+                permutation = permVector.toString();
+            }
+            System.out.print(permutation);
         }
 
     }
 }
+
+
