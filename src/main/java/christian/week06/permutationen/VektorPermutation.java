4398520ebb58dd591d3a35c9ca320b04012752f7
schmid <schmid100@hotmail.com>
2024-04-14 21:29:11 +0200

    -permutation immer noch nicht fertig XD

--- src/main/java/christian/week06/permutationen/VektorPermutation.java
+++ src/main/java/christian/week06/permutationen/VektorPermutation.java
@@ -7,23 +7,22 @@ public class VektorPermutation {
     public static void main(String[] args) {
         Vector<String> vektor1 = new Vector<String>();
         Vector<String> vektor2 = new Vector<String>(Arrays.asList("A", "B", "C", "D"));
-//        permutate(vektor1, vektor2);
-        permutate2(vektor2, 0);
+        permutate(vektor1, vektor2);
+//        permutate2(vektor2, 0);
     }
 
     public static void permutate(Vector<String> vector, Vector<String> leftOver) {
-        String permutation = "";
-        Vector<String> permVector = vector;
-        for (int i = 0; i < leftOver.size(); i++) {
-            permVector.add(leftOver.remove(i));
-            if (!leftOver.isEmpty()) {
-                permutate(permVector, leftOver);
-            } else {
-                permutation = permVector.toString();
-                System.out.print(permutation);
+        Vector<String> copyOfVector = new Vector<>(vector);
+        Vector<String> copyOfLeftOver = new Vector<>(leftOver);
+        Vector<String> permVector = copyOfVector;
+
+        while (!copyOfLeftOver.isEmpty()) {
+            for (int i = 0; i < copyOfLeftOver.size(); i++) {
+                permVector.add(copyOfLeftOver.remove(i));
+                permutate(permVector, copyOfLeftOver);
             }
         }
-
+        System.out.println(permVector.toString());
     }
 
     public static void permutate2(Vector<String> vector, int fixedIndex) {
@@ -32,12 +31,9 @@ public class VektorPermutation {
                 System.out.println(vector.toString());
             } else {
                 vector.set(fixedIndex, vector.get(i));
-
-                permutate2(vector, fixedIndex);
+                permutate2(vector, fixedIndex++);
             }
-            fixedIndex++;
-
-
+        }
     }
 }
 
