b9d36dd072622af8d7beb1b31ffc8e342e396b8b
Christian Schmid <schmid100@hotmail.com>
2024-04-14 19:34:58 +0200

    -permutation immer noch nicht fertig

--- src/main/java/christian/week06/permutationen/VektorPermutation.java
+++ src/main/java/christian/week06/permutationen/VektorPermutation.java
@@ -7,21 +7,36 @@ public class VektorPermutation {
     public static void main(String[] args) {
         Vector<String> vektor1 = new Vector<String>();
         Vector<String> vektor2 = new Vector<String>(Arrays.asList("A", "B", "C", "D"));
-        permutate(vektor1, vektor2);
+//        permutate(vektor1, vektor2);
+        permutate2(vektor2, 0);
     }
 
     public static void permutate(Vector<String> vector, Vector<String> leftOver) {
         String permutation = "";
+        Vector<String> permVector = vector;
         for (int i = 0; i < leftOver.size(); i++) {
-            Vector<String> permVector = new Vector<String>();
             permVector.add(leftOver.remove(i));
             if (!leftOver.isEmpty()) {
                 permutate(permVector, leftOver);
             } else {
                 permutation = permVector.toString();
+                System.out.print(permutation);
             }
-            System.out.print(permutation);
         }
+
+    }
+
+    public static void permutate2(Vector<String> vector, int fixedIndex) {
+        for (int i = fixedIndex; i < vector.size(); i++) {
+            if (fixedIndex == vector.size() - 1) {
+                System.out.println(vector.toString());
+            } else {
+                vector.set(fixedIndex, vector.get(i));
+
+                permutate2(vector, fixedIndex);
+            }
+            fixedIndex++;
+
 
     }
 }
