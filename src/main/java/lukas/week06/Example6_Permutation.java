f7dec13fd05df20550102981dfbd552960843f93
LZE <lukas.zech@gmx.at>
2024-04-15 16:11:45 +0200

    permutation

--- src/main/java/lukas/week06/Example6_Permutation.java
+++ src/main/java/lukas/week06/Example6_Permutation.java
@@ -11,25 +11,62 @@ public class Example6_Permutation {
         vec.add("C");
         vec.add("D");
 
+        System.out.println("----VERSION1----");
         getPermutations(vec, new Vector<String>(), "");
+        System.out.println("----VERSION2----");
+        getPermutationsV2(vec, "");
+        System.out.println("----VERSION3----");
+        getPermutationsV3(vec, "");
 
     }
 
 
-
-
-    public static void getPermutations(List<String> elements, List<String> generations, String currentPermutation) {
-        List<String> remainingElements = new Vector<>(elements);
-        if(!elements.isEmpty()) {
-            for(String s : elements) {
-                String nextPermutation = currentPermutation + s;
-                remainingElements.remove(s);
-                getPermutations(remainingElements, generations, nextPermutation);
-                System.out.printf("s: %s - currP: %4s - nextP: %4s\n", s, nextPermutation, currentPermutation);
+    public static void getPermutations(List<String> remaining, List<String> permutations, String prefix) {
+        System.out.println(prefix+"r: " + remaining);
+        System.out.println(prefix+"p: " + permutations);
+        if (!remaining.isEmpty()) {
+            for (int i = 0; i < remaining.size(); i++) {
+                String current = remaining.get(i);
+                Vector<String> newRemaining = new Vector<>(remaining);
+                newRemaining.remove(i);
+                Vector<String> newPermutations = new Vector<>(permutations);
+                newPermutations.add(current);
+                //System.out.println(prefix + "current: " + current);
+                getPermutations(newRemaining, newPermutations, prefix + "->");
             }
-        }else {
-            generations.add(currentPermutation);
-            System.out.println(currentPermutation);
+        } else {
+            //System.out.println("----------");
+            System.out.println(permutations);
+            //System.out.println();
+        }
+
+    }
+
+    public static void getPermutationsV2(List<String> remaining, String permutation) {
+        if (!remaining.isEmpty()) {
+            for (int i = 0; i < remaining.size(); i++) {
+                String current = remaining.get(i);
+                Vector<String> newRemaining = new Vector<>(remaining);
+                newRemaining.remove(i);
+                String newTest = permutation + current;
+                getPermutationsV2(newRemaining, newTest);
+            }
+        } else {
+            System.out.println(permutation);
+        }
+
+    }
+
+    public static void getPermutationsV3(List<String> remaining, String permutation) {
+        if (!remaining.isEmpty()) {
+            for (String s : remaining) {
+                Vector<String> newRemaining = new Vector<>(remaining);
+                newRemaining.remove(s);
+                String newTest = permutation + s;
+                getPermutationsV2(newRemaining, newTest);
+            }
+        } else {
+            System.out.println(permutation);
         }
 
     }
