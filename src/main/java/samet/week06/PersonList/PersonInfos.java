c64890bafeaf990eba89d542c14d7ee2887dee1c
samy0307 <samet.sevimli07@gmail.com>
2024-04-15 08:31:13 +0200

    PersonV2

--- src/main/java/samet/week06/PersonList/PersonInfos.java
+++ src/main/java/samet/week06/PersonList/PersonInfos.java
@@ -9,51 +9,44 @@ public class PersonInfos {
     private String name;
     private double weight;
 
-
     public PersonInfos(int size, int age, String name, double weight) {
 
         this.size = size;
         this.age = age;
         this.name = name;
         this.weight = weight;
-
     }
 
     public int getSize() {
         return size;
     }
 
-    public void setSize(int size) {
-        this.size = size;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getWeight() {
         return weight;
     }
 
-    public void setWeight(double gewicht) {
-        this.weight = weight;
-    }
 
-
+    public String toString() { //to String damit die Ausgabe schön aussieht!
+        return String.format("%-12s (%2d) %8.1f kg / %3d cm", name, age, weight, size);
 
     }
+}
+
+
+
+
+
+
+
+
 
 
 
