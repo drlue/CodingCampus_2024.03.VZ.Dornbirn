21291e5346350318e784bab258863a67f9e49e5f
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-04-16 09:32:36 +0200

    Mein Zoo macht! Es fehlt jetzt nur noch das Personal.

--- src/main/java/katherina/week7/day01/zoo/Gehege.java
+++ src/main/java/katherina/week7/day01/zoo/Gehege.java
@@ -5,24 +5,14 @@ import java.util.Vector;
 public class Gehege {
     private String name;
     private Zoo zoo;
-    private Tier tier;
 
-    //Ich möchte für jeden Gehegetyp einen eigenen Vector anlegen, weil ich das innerweltlich logischer finde:
-    //Ich kann zwar eine Kuh auch ins Ried stellen und einen Storch ins Terrarium - oder gar Aquarium!
-    //Aber klug ist das nicht. Also lassen wir das besser sein ;-)
-    //Es ist informatisch einen Tick unflexibel, aber wie gesagt... realistischer.
-
-    private Vector<Tier> wiesentierliste;
-    private Vector<Tier> riedtierliste;
-    private Vector<Tier> terrariumListe;
+    private Vector<Tier> tierliste;
 
 
     public Gehege(String name) {
         this.name = name;
         this.zoo = null;
-        wiesentierliste = new Vector<>();
-        riedtierliste = new Vector<>();
-        terrariumListe = new Vector<>();
+        tierliste = new Vector<>();
     }
 
     public void setName(String name) {
@@ -45,39 +35,16 @@ public class Gehege {
         this.zoo = zoo;
     }
 
-    public void setTier(Tier tier) {
-        if (tier != null) {
-            if (tier.getGehege() != null) {
-                tier.getGehege().tier = null;
-            }
-            tier.setGehegeIntern(this);
-        }
-        this.tier = tier;
-    }
 
-    public Tier getTier() {
-        return tier;
-    }
-
-    public void addWiesentier(Tier tier) {
+    public void addTier(Tier tier) {
         if (tier != null) {
             tier.setGehegeIntern(this);
         }
-        wiesentierliste.add(tier);
+        tierliste.add(tier);
     }
 
-    public void addRiedTier(Tier tier) {
-        if (tier != null) {
-            tier.setGehegeIntern(this);
-        }
-        riedtierliste.add(tier);
-    }
-
-    public void addTerrariumWarmesTier(Tier tier) {
-        if (tier != null) {
-            tier.setGehegeIntern(this);
-        }
-        terrariumListe.add(tier);
+    public void removeTier(Tier tier){
+        tierliste.remove(tier);
     }
 
     public void addPersonal(Pfleger pfleger) {
@@ -93,32 +60,18 @@ public class Gehege {
     public String toString() {
         String gehegestring = "";
         gehegestring = "    ├── Gehege: " + name + "\n";
-        if (wiesentierliste != null) {
-            for (Tier ausgabe : wiesentierliste) {
+        if (tierliste != null) {
+            for (Tier ausgabe : tierliste) {
                 gehegestring += ausgabe;
             }
         }
-        if (wiesentierliste == null) {
-            gehegestring += "(leer2)";
-        }
-        if (riedtierliste != null) {
-            for (Tier ausgabe : riedtierliste) {
-                gehegestring += ausgabe;
-            }
-        }
-        if (riedtierliste == null) {
-            gehegestring += "(leer)";
-        }
-        if (terrariumListe != null) {
-            for (Tier ausgabe : terrariumListe) {
-                gehegestring += ausgabe;
-            }
-        }
-        if (terrariumListe == null) {
-            gehegestring += "├──(leer)";
+        if (tierliste.isEmpty()) {
+            gehegestring += "        ├── dieses Gehege enthält keine Tiere. Bitte füge Tiere hinzu!";
         }
         return gehegestring;
     }
+
+
 }
 
 
