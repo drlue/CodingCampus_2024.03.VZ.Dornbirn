0bdcc4e951c712dcce2b3580bf633e6018735a2e
Gyula Horvath <gylhrvth@gmail.com>
2024-04-16 15:04:40 +0200

    Add Guardian to Zoo.

--- src/main/java/gyula/week07/zoo/Zoo.java
+++ src/main/java/gyula/week07/zoo/Zoo.java
@@ -6,11 +6,20 @@ public class Zoo {
     private String name;
     private int established;
     private Vector<Enclosure> enclosureList;
+    private Vector<Guardian> guardianList;
 
     public Zoo(String name, int established){
         this.name = name;
         this.established = established;
         enclosureList = new Vector<>();
+        guardianList = new Vector<>();
+    }
+
+    public void addGuardianAndTask(Guardian gua, Enclosure enc){
+        gua.addTask(enc);
+        if (!guardianList.contains(gua)){
+            guardianList.add(gua);
+        }
     }
 
     public void addEnclosure(Enclosure enc){
@@ -23,6 +32,9 @@ public class Zoo {
 
     public void printStructure(){
         System.out.printf("|-- Zoo: %s, gegrundet %d%n", name, established);
+        for (Guardian gua : guardianList){
+            gua.printStructure();
+        }
         for (Enclosure enc : enclosureList){
             enc.printStructure();
         }
