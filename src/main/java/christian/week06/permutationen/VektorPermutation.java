b465b60044864bac80ac24a92ae894435fa39c10
Christian Schmid <schmid100@hotmail.com>
2024-04-16 19:11:44 +0200

    permutation von Gyula abgeschrieben

--- src/main/java/christian/week06/permutationen/VektorPermutation.java
+++ src/main/java/christian/week06/permutationen/VektorPermutation.java
@@ -4,57 +4,48 @@ import java.util.Arrays;
 import java.util.Vector;
 
 public class VektorPermutation {
-//    public static void main(String[] args) {
-//        Vector<String> leererVektor = new Vector<String>();
-//        Vector<String> inputVektor = new Vector<String>(Arrays.asList("A", "B", "C", "D"));
-////        permutate(inputVektor);
+    public static void main(String[] args) {
+        Vector<String> leererVektor = new Vector<String>();
+        Vector<String> inputVektor = new Vector<String>(Arrays.asList("A", "B", "C", "D"));
+
+        System.out.println(permutate(leererVektor, inputVektor));
 //        permutate2(inputVektor, 0, inputVektor.size(), inputVektor.size());
-//    }
-//
-//    public static void permutate(Vector<String> remainingLetters) {
-//        Vector<String> permVektor = new Vector<>();
-//
-//        for (int i = 0; i < remainingLetters.size(); i++) {
-//
-//            permVektor.add(remainingLetters.get(i));
-//
-//            permutate(permVektor, remainingLetters);
-//            remainingLetters.remove(i);
-//
-//            permVektor.remove(i);
-//
-//        }
-//
-//        System.out.println(permVektor.toString());
-//    }
-//
-//    public static void permutate(Vector<String> permVektor, Vector<String> remainingLetters) {
-//
-//        for (int i = 0; i < remainingLetters.size(); i++) {
-//            permVektor.add(remainingLetters.remove(i));
-//
-//            permutate(permVektor, remainingLetters);
-//
-//        }
-//
-//        System.out.println(permVektor.toString());
-//    }
-//
-//    public static void permutate2(Vector<String> vector, int fixedIndex, int size, int orginalSize) {
-//        for (int i = fixedIndex; i < vector.size(); i++) {
-//            if (fixedIndex == orginalSize) {
-//                System.out.println(vector.toString());
-//            } else if (fixedIndex > size && fixedIndex < orginalSize - 1) {
-//                String temp = vector.set(fixedIndex + 1, vector.get(size - 1));
-//                vector.set(size - 1, temp);
-//            } else {
-//
-//                size--;
-//                permutate2(vector, ++fixedIndex, size, orginalSize);
-//                System.out.println(vector.toString());
-//            }
-//        }
-//    }
+    }
+
+
+    public static Vector<Vector<String>> permutate(Vector<String> permVektor, Vector<String> remainingLetters) {
+        Vector<Vector<String>> all = new Vector<>();
+        if(remainingLetters.isEmpty()){
+            all.add(permVektor);
+            return all;
+        }
+        for (int i = 0; i < remainingLetters.size(); i++) {
+            Vector<String> newPermVector = new Vector<>(permVektor);
+            Vector<String> newRemainingLetters = new Vector<>(remainingLetters);
+            newPermVector.add(newRemainingLetters.remove(i));
+
+            all.addAll(permutate(newPermVector, newRemainingLetters));
+
+        }
+
+        return all;
+    }
+
+    public static void permutate2(Vector<String> vector, int fixedIndex, int size, int orginalSize) {
+        for (int i = fixedIndex; i < vector.size(); i++) {
+            if (fixedIndex == orginalSize) {
+                System.out.println(vector.toString());
+            } else if (fixedIndex > size && fixedIndex < orginalSize - 1) {
+                String temp = vector.set(fixedIndex + 1, vector.get(size - 1));
+                vector.set(size - 1, temp);
+            } else {
+
+                size--;
+                permutate2(vector, ++fixedIndex, size, orginalSize);
+                System.out.println(vector.toString());
+            }
+        }
+    }
 }
 
 
