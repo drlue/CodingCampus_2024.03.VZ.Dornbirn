ae244915970b5f5dbc9d3ab70aa4cffea0f4f6ba
Demian <demian.fitz@gmail.com>
2024-04-19 11:23:25 +0200

    -update

--- src/main/java/demian/week08/Enum/Pizza/Sim.java
+++ src/main/java/demian/week08/Enum/Pizza/Sim.java
@@ -5,6 +5,10 @@ public class Sim {
     public static void main(String[] args) {
 
         Pizza salamipizza = new Pizza();
-        
+        salamipizza.addTopping(PizzaTopings.SALAMI);
+        salamipizza.addTopping(PizzaTopings.CHEESE);
+        salamipizza.addTopping(PizzaTopings.TOMATO);
+        salamipizza.backingSIM();
+        System.out.println("Preis für Pizza mit " + salamipizza.getToppinganzahl() + " Toppings (" + salamipizza.getToppings() + "): "+ salamipizza.calculatePizzaPrice());
     }
 }
