3ca065948d35e3f1856159c2ba63d16ed00ea625
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-04-24 10:12:17 +0200

    Ich habe drei sehr verschiedene Testautos (und einen "Crashdummy" für besonders kleine Tests gebaut). Was bereits funktioniert: Fahrt simulieren und zu gucken, ob das Auto die Strecke schafft. Was noch nicht klappt: Dass das getankte Auto so lange fährt, bis es erneut getankt werden muss.

--- src/main/java/katherina/week08/Car/Car.java
+++ src/main/java/katherina/week08/Car/Car.java
@@ -3,37 +3,74 @@ package katherina.week08.Car;
 public class Car {
     private String hersteller;
     private String modell;
-    private int kiloWatt;
-    private int maxTankinhalt;
-    private int tankinhalt;
-    private int gewicht;
+    private double kiloWatt;
+    private double tankinhalt;
+    private double gewicht;
 
-    private Antriebsart antriebsart;
-
-    private enum Antriebsart {
+    enum Antriebsart {
         BENZIN,
         DIESEL,
         GAS,
         STROM
     }
 
-    public Car(String hersteller,String modell,int kiloWatt,int tankinhalt,int gewicht, Antriebsart antriebsart){
-        this.hersteller=hersteller;
-        this.modell=modell;
-        this.kiloWatt=kiloWatt;
-        this.tankinhalt=tankinhalt;
-        this.gewicht=gewicht;
-        this.antriebsart=antriebsart;
-        this.maxTankinhalt=maxTankinhalt;
+    private Antriebsart antriebsart;
+
+    public Car(String hersteller, String modell, double kiloWatt, double tankinhalt, double gewicht, Antriebsart antriebsart) {
+        this.hersteller = hersteller;
+        this.modell = modell;
+        this.kiloWatt = kiloWatt;
+        this.tankinhalt = tankinhalt;
+        this.gewicht = gewicht;
+        this.antriebsart = antriebsart;
     }
 
-    public int drive(int kilometer){
-//      //  int verfahren = reichweite - kilometer/tankinhalt;
-//        int tankinhalt = maxTankinhalt-(tankinhalt/kilometer*100);
-//        int reichweite = maxTankinhalt*verfahren/tankinhalt;
-//        if (){
-//
-//        }
-        return kilometer;
+    public void setTankinhalt(double tankinhalt) {
+        this.tankinhalt = tankinhalt;
     }
+
+    public int drive(int kilometer) {
+        System.out.print("START||");
+        double strecke = 0;
+        while (tankinhalt > 0 && kilometer > 0) {
+            verbrauchProKM();
+            //Unterfunktion: Immer wenn ich den nächsten Kilometer gefahren bin, prüfe ich: Kann ich weiterfahren?
+            //Wenn ja: Drucke =
+            //Wenn nein: Drucke [Box]. Tanke auf. Fahre weiter.
+            kilometer--;
+            strecke++;
+            System.out.print("=");
+        }
+        if (tankinhalt <= 0) {
+            auffuellen(tankinhalt);
+            setTankinhalt(tankinhalt);
+            verbrauchProKM();
+            System.out.print("|[]=[]> ENDE||\n");
+        } else {
+            System.out.printf("|[]=[]> ENDE||\n%nWir sind nach %.2f km angekommen!%n Es sind noch %.2f Liter Kraftstoff übrig.%n", strecke, tankinhalt);
+        }
+
+        return (int) strecke;
+    }
+
+    public void auffuellen(double fuellung) {
+        System.out.print("[Box]");
+        setTankinhalt(tankinhalt);
+        fuellung = 25;
+        tankinhalt += fuellung;
+        verbrauchProKM();
+    }
+
+    public void verbrauchProKM() {
+        double strecke = 0;
+        switch (antriebsart) {
+            case GAS -> tankinhalt -= ((gewicht / kiloWatt) / 1.1) / 100;
+            case BENZIN -> tankinhalt -= ((gewicht / kiloWatt) / 1.2) / 100;
+            case DIESEL -> tankinhalt -= ((gewicht / kiloWatt) / 1.3) / 100;
+            case STROM -> tankinhalt -= ((gewicht / kiloWatt) / 1.6) / 100;
+        }
+        System.out.print("=");
+    }
+
 }
+
---
+++ src/main/java/katherina/zoocopyforexperiments/featureIdeen.md
@@ -0,0 +1,33 @@
+24.04.2024
+
+### **Geplante Features**
+
+- Futterautomatik (Futterverbrauch je Tier, Futtersorten etc. - und Berechnung des Verbrauchs pro Tag bzw. der Kosten.)
+- MOOC-Feature: Von Tag zu Tag soll eine zufällige Anzahl an Besuchenden generiert werden. Diese sollen folgende Eigenschaften aufweisen: Vorname, Nachname, Alter, Kaufkraft, Liebling.
+- Die Besuchenden sollen folgende Dinge tun: Sie sollen die Gehege durchlaufen und Tiere anschauen. Ist das zufällig angeschaute Lieblingstier und haben sie genug Kaufkraft, so sollen sie Tierfutter für 1€ kaufen.
+
+
+#### Spätere Bonusfeatures für Besuchende:
+
+- Softeisautomat - falls nach dem Durchlaufen aller Gehege noch Geld vorhanden ist, soll ein Softeis für 2€ aus dem Automaten gezogen werden.
+- Zoo-Giftshop - falls nach dem Kauf eines Softeises noch Geld vorhanden ist, kann eine Postkarte (1 €) oder ein Plüschtier, klein (5 €) oder ein Plüschtier, groß (25€) gekauft werden.
+- Statt eines Giftshops (oder davor, danach, ich muss das noch überlegen) können die Leute auch einkehren und Pommes (2€) und einen Softdrink (1€) kaufen.
+
+Am Ende soll ausgegeben werden, ob der Zoo rote oder schwarze Zahlen geschrieben hat.
+
+#### **Weitere Möglichkeiten, die Komplexität des Zoos zu erhöhen:**
+
+- Events (beispielsweise: Eine Schulklasse besucht den Zoo und gibt 300€ aus! oder "Aufgrund einer Unwetterwarnung muss der Zoo heute geschlossen bleiben. Es gibt an diesem Tag keine Besuchenden").
+- Automatisierter Anbau von Gehegen (beispielsweise "Sobald der Zoo 10.000 Euro Gewinn gemacht hat, wird ein Affenhaus gebaut und automatisch mit so und so vielen Affen gefüllt, die wiederum bestimmte Bedürfnisse haben. Leute, deren Lieblingstier ein Affe ist, geben nun mehr Geld aus.)
+- Gehalt für die Pflegenden und Honorar für den Tierarzt, sofern der Tierarzt ein Tier heilen musste. (also fast jeden Tag)
+
+#### **Komfort-Features und Usability**
+
+- Es ist geplant, auf lange Sicht die Möglichkeit eines Safe-Files zu implementieren. Nach jeder Simulation (Anzahl der festgelegten Schleifen in "day") wird in eine Datei gespeichert und anschließend aus dieser Datei gelesen, um anschließend erneut die gleiche Anzahl an Tagen zu simulieren. Die neue Ausgabe wird ebenfalls im Safe-File hinterlegt.
+- Damit einhergehend ist geplant, eine Abfrage einzubauen, ob "from scratch" simuliert werden soll oder ein Savefile geladen werden soll.
+- Accessibility: Auf lange Sicht möchte ich die Möglichkeit einbauen, eine Ausgabe mit Markdown, eine als .txt und eine gefärbte in der Konsole zu realisieren.
+
+#### **Verbesserungen**
+
+- "Die Tiere in diesem Gehege sind brav." soll nur noch einmal ausgegeben werden, nämlich für den Fall, dass kein einziger Biss im Gehege ausgelöst wird.
+- die Ausgabe bei leeren Gehegen fehlt.
