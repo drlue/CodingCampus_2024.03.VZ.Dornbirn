3e18498470e495efee779b43e3bc93e349f24235
Christian Schmid <schmid100@hotmail.com>
2024-04-24 15:39:08 +0200

    carSimulation

--- src/main/java/christian/week07/carSimulationRemastered/Car.java
+++ src/main/java/christian/week07/carSimulationRemastered/Car.java
@@ -3,17 +3,23 @@ package christian.week07.carSimulationRemastered;
 public class Car {
     private String hersteller;
     private String modell;
+    private double weight;
     private Engine engine;
     private Tank tank;
 
-    public Car(String hersteller, String modell, Engine engine, Tank tank) {
+    public Car(String hersteller, String modell, double weight, Engine engine, Tank tank) {
         this.hersteller = hersteller;
         this.modell = modell;
+        this.weight = weight;
         this.engine = engine;
         this.tank = tank;
     }
+
     //==================================================================================================================get
     //==================================================================================================================set
     //==================================================================================================================alter
     //==================================================================================================================simulate
+    public double FuelUsagePerKilometer() {
+        return (weight * 0.004 + engine.getkW() * 0.02) / 100;
+    }
 }
--- src/main/java/christian/week07/carSimulationRemastered/Engine.java
+++ src/main/java/christian/week07/carSimulationRemastered/Engine.java
@@ -3,12 +3,28 @@ package christian.week07.carSimulationRemastered;
 public class Engine {
     private double kW;
     private int kilometerCounter;
-    enum DriveType {GASOLINE,DIESEL,GAS,ELECTRIC}
+
+    enum DriveType {GASOLINE, DIESEL, GAS, ELECTRIC}
+
     private DriveType driveType;
-    enum Status {BROKE,INTACTRUNNING,INTACTSTOP}
+
+    enum Status {BROKE, INTACTRUNNING, INTACTSTOP}
+
     private Status status;
 
-    public Engine(double kW,DriveType driveType){
-
+    public Engine(double kW, DriveType driveType) {
+        this.kW = kW;
+        this.kilometerCounter = 0;
+        this.driveType =driveType;
+        this.status = Status.INTACTSTOP;
     }
+    //==================================================================================================================get
+
+    public double getkW() {
+        return kW;
+    }
+
+    //==================================================================================================================set
+    //==================================================================================================================alter
+    //==================================================================================================================simulate
 }
