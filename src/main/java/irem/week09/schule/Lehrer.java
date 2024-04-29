2c4f0554d9a4fe67b3218222ca7360bf98462042
Irem <aksoy.irem5757@gmail.com>
2024-04-29 14:18:44 +0200

    Woche 9 Schule

---
+++ src/main/java/irem/week09/schule/Lehrer.java
@@ -0,0 +1,7 @@
+package irem.week09.schule;
+
+class Lehrer extends Person {
+    public Lehrer(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/irem/week09/schule/Raum.java
@@ -0,0 +1,36 @@
+package irem.week09.schule;
+
+
+enum RaumTyp {
+    KLASSENRAUM, SPORTHALLE, TECHNIKRAUM, LEHRERZIMMER, DIREKTORZIMMER
+}
+class Raum {
+    private RaumTyp typ;
+    private String name;
+
+    public Raum(RaumTyp typ, String name) {
+        this.typ = typ;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public RaumTyp getTyp() {
+        return typ;
+    }
+
+    // Methode zur Überprüfung des Zugriffs auf den Raum
+    public boolean hatZugriff(Person person) {
+        if (typ == RaumTyp.LEHRERZIMMER || typ == RaumTyp.DIREKTORZIMMER) {
+            return person instanceof Lehrer || person instanceof Schuldirektor;
+        }
+        return true;  // Alle anderen Räume sind für alle Personen zugänglich
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + typ + ")";
+    }
+}
