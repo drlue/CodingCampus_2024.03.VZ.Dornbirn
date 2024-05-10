0daaa08b969e9cafe783b5881146783ab80d449e
LZE <lukas.zech@gmx.at>
2024-05-10 12:39:42 +0200

    sql query print data table

--- src/main/java/lukas/week10/SqlQuery/SqlQuery.java
+++ src/main/java/lukas/week10/SqlQuery/SqlQuery.java
@@ -1,28 +1,22 @@
 package lukas.week10.SqlQuery;
 
 import ardijanla.ConsoleColors;
-import lukas.Helper;
+import lukas.Colors;
 
 import java.sql.*;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Scanner;
 
 public class SqlQuery {
 
 
     public static void main(String[] args) {
-
         //User Input
         //String myCountry = Helper.readStringFromConsole("Bitte ein Land eingeben:==>");
-
 
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
 
-
-        // String myCountry = "Austria";
 
         try {
             //establish connection to mysql server
@@ -41,6 +35,8 @@ public class SqlQuery {
             //prepare and execute SQL statement
             stmt = conn.prepareStatement(sql);
             //stmt.setString(1, myCountry);
+            //stmt.setString(1, myCountry);
+
             rs = stmt.executeQuery();
 
             //OUTPUT
@@ -77,29 +73,32 @@ public class SqlQuery {
 
     public static void printTable(ResultSet rs) throws SQLException {
         //Color Definitions
-        String headerBackground = "";
-        String headerForeground = "";
-        String oddRowBackground = "";
-        String oddRowForeground = "";
-        String evenRowBackground = "";
-        String evenRowForeground = "";
+        String headerColors = Colors.hslForeAndBackground(0, 1, 1.0f, 240, 0.059f, 0.333f);
+        headerColors = Colors.makeFontBold(headerColors);
+        String oddRowColors = Colors.hslForeAndBackground(0, 1, 0.0f, 240, 0.0f, 0.949f);
+        String evenRowColors = Colors.hslForeAndBackground(0, 1, 0.0f, 240, 0.0f, 0.910f);
+//        String headerColors = Colors.rgbForeAndBackground(255,255,255,80,80,90);
+//        String oddRowColors = Colors.rgbForeAndBackground(1,1,1,242,242,242);
+//        String evenRowColors = Colors.rgbForeAndBackground(1,1,1,232,232,232);
+
 
 
         ResultSetMetaData metaData = rs.getMetaData();
+
         int tabWidth = 0;
         //print header
         //Set Backgroundcolors:
+        System.out.print(headerColors);
 
-        System.out.print("\033[38;2;10;10;10;48;2;137;143;153m");
         for (int col = 1; col <= metaData.getColumnCount(); col++) {
             int colSize = Math.max(metaData.getColumnDisplaySize(col), metaData.getColumnLabel(col).length());
             System.out.printf("%-" + colSize + "s | ", metaData.getColumnLabel(col));
-            tabWidth += metaData.getColumnDisplaySize(col) + 3;
+            tabWidth += colSize + 3;
         }
         System.out.print(ConsoleColors.RESET);
         System.out.println();
 
-        System.out.print("\033[38;2;10;10;10;48;2;137;143;153m");
+        System.out.print(headerColors);
         System.out.println("-".repeat(tabWidth) + ConsoleColors.RESET);
 
         //print data rows
@@ -107,7 +106,7 @@ public class SqlQuery {
         while (rs.next()) {
 
             //alternating background colors per line
-            System.out.print(row % 2 == 0 ? "\033[38;2;10;10;10;48;2;237;243;253m" : "\033[38;2;10;10;10;48;2;187;193;203m");
+            System.out.print(row % 2 == 0 ? evenRowColors : oddRowColors);
 
             //iterate columns
             for (int col = 1; col <= metaData.getColumnCount(); col++) {
@@ -138,84 +137,6 @@ public class SqlQuery {
             row++;
         }
 
-    }
-
-
-
-    public static String convertRgbToAnsiForeground(int[] rgb) {
-        return "\\033[38;2;rgb[0];rgb[1];rgb[2]m";
-    }
-
-    public static String convertRgbToAnsiBackground(int[] rgb) {
-        return "\\033[48;2;rgb[0];rgb[1];rgb[2]m";
-    }
-
-    public static String convertRgbToAnsiForeground(int r, int g, int b) {
-        int[] rgb = new int[]{r,g,b};
-        return convertRgbToAnsiForeground(rgb);
-    }
-
-    public static String convertRgbToAnsiBackground(int r, int g, int b) {
-        int[] rgb = new int[]{r,g,b};
-        return convertRgbToAnsiBackground(rgb);
-    }
-
-    public static String convertRgbToAnsiBackgroundAndForeground(int[] foregroundRgb, int[] backgroundRgb) {
-        return "\\033[38;2;foregroundRgb[0];foregroundRgb[1];foregroundRgb[2];48;2;backgroundRgb[0];backgroundRgb[1];backgroundRgb[2]m";
-    }
-
-
-    public static String convertHslToAnsiForeground(int h, float s, float l) {
-        int[] foregroundRgb = convertHslToRgb(h, s, l);
-        return "\\033[38;2;foregroundRgb[0];foregroundRgb[1];foregroundRgb[2]m";
-    }
-
-    public static String convertHslToAnsiBackground(int h, float s, float l) {
-        int[] backgroundRgb = convertHslToRgb(h, s, l);
-        return "\\033[48;2;backgroundRgb[0];backgroundRgb[1];backgroundRgb[2]m";
-    }
-
-    public static String convertHslToAnsiBackgroundAndForeground(int hFore, float sFore, float lFore,int hBack, float sBack, float lBack) {
-        int[] foregroundRgb = convertHslToRgb(hFore, sFore, lFore);
-        int[] backgroundRgb = convertHslToRgb(hBack, sBack, lBack);
-        return "\\033[38;2;foregroundRgb[0];foregroundRgb[1];foregroundRgb[2];48;2;backgroundRgb[0];backgroundRgb[1];backgroundRgb[2]m";
-    }
-
-    public static int[] convertHslToRgb(int h, float s, float l) {
-        //convert hsl to rgb
-        float r, g, b;
-
-        if (s == 0) { //achromatic
-            r = l;
-            g = l;
-            b = l;
-        } else {
-            float q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            float p = 2 * l - q;
-            r = hueToRgb(p, q, h + 1f/3f);
-            g = hueToRgb(p, q, h);
-            b = hueToRgb(p, q, h - 1f/3f);
-
-        }
-        //int[] rgb = {Math.round(r*255), Math.round(g*255), Math.round(b*255)};
-        int[] rgb = {to255(r), to255(g), to255(b)};
-        return rgb;
-    }
-
-    public static int to255(float v) { return (int)Math.min(255,256*v); }
-    public static float hueToRgb(float p, float q, float t){
-        if(t < 0) {
-            t += 1;
-        }else if(t > 1) {
-            t -= 1;
-        }else if(t < (1f/6f)) {
-            return p + (q - p) * 6f * t;
-        }
-        if (t < 1f/2f)
-            return q;
-        if (t < 2f/3f)
-            return p + (q - p) * (2f/3f - t) * 6f;
-        return p;
     }
 
 
