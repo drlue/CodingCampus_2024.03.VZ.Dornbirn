7588d7f7f7deb890ae0c11aabe2d44e8461d2073
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-05-15 11:44:48 +0200

    Java meets MySQL

--- src/main/java/katherina/week11/sqlBank/Customer.java
+++ src/main/java/katherina/week11/sqlBank/Customer.java
@@ -1,5 +1,6 @@
 package katherina.week11.sqlBank;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,8 @@ public class Customer {
     private String surname;
     private float balance;
     private List<Customer> customerList;
+
+    public Customer(){}
 
     public Customer(String name, String surname,float balance){
         this.name=name;
@@ -21,6 +24,54 @@ public class Customer {
         if (!customerList.contains(customer)){
             customerList.add(customer);
         }
+    }
+
+//    public void create(Customer customer) throws SQLException {
+//        try (
+//                Connection connection = dataSource.getConnection();
+//                PreparedStatement statement = connection.prepareStatement(SQL_INSERT,
+//                        Statement.RETURN_GENERATED_KEYS);
+//        ) {
+//
+//            statement.setString(1, String.valueOf(customer.getId()));
+//            statement.setString(2, customer.getName());
+//            statement.setString(3, customer.getSurname());
+//            statement.setString(4, String.valueOf(customer.getBalance()));
+//
+//            int affectedRows = statement.executeUpdate();
+//
+//            if (affectedRows == 0) {
+//                throw new SQLException("Creating user failed, no rows affected.");
+//            }
+//
+//            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+//                if (generatedKeys.next()) {
+//                    customer.setId(generatedKeys.getLong(1));
+//                }
+//                else {
+//                    throw new SQLException("Creating user failed, no ID obtained.");
+//                }
+//            }
+//        }
+//    }
+
+    private float getBalance() {
+        return balance;
+    }
+
+    private int getId() {
+        return iD;
+    }
+
+    private String getSurname() {
+        return surname;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private void setId(long aLong) {
     }
 
     public void setName(String name) {
