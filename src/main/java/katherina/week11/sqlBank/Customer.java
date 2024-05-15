4e0b6ad049ba55874425bf589c7d98c15275d7cc
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-05-15 10:47:30 +0200

    Java meets MySQL

--- src/main/java/katherina/week11/sqlBank/Customer.java
+++ src/main/java/katherina/week11/sqlBank/Customer.java
@@ -1,10 +1,27 @@
 package katherina.week11.sqlBank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
     private int iD;
     private String name;
     private String surname;
     private float balance;
+    private List<Customer> customerList;
+
+    public Customer(String name, String surname,float balance){
+        this.name=name;
+        this.surname=surname;
+        this.balance=balance;
+        this.customerList=new ArrayList<>();
+    }
+
+    public void addCustomer(Customer customer){
+        if (!customerList.contains(customer)){
+            customerList.add(customer);
+        }
+    }
 
     public void setName(String name) {
         this.name=name;
