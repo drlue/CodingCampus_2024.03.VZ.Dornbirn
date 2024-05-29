78a7019eb263d5a7365d560be80dc87e0082824a
LZE <lukas.zech@gmx.at>
2024-05-29 09:02:49 +0200

    blabla

--- html/lukas/Javascript/toDoList/toDoListJS.js
+++ html/lukas/Javascript/toDoList/toDoListJS.js
@@ -5,7 +5,7 @@ let updateIndex = '';
 
 //get toDoList from local storage
 let toDoArray = JSON.parse(localStorage.getItem('ToDoList'));
-if (toDoArray === null){
+if (toDoArray === null) {
     toDoArray = [];
 }
 
@@ -14,11 +14,11 @@ document.querySelector("#inputDate").valueAsDate = new Date();
 
 displayToDoList();
 
-function addItem(){
-    
+function addItem() {
+
     let inputTxt = document.querySelector("#inputBox").value;
     let inputDate = document.querySelector("#inputDate").value;
-    console.log("1: "+ inputTxt + " 2: " + inputDate);
+    console.log("1: " + inputTxt + " 2: " + inputDate);
 
     let inputItem = {
         text: inputTxt,
@@ -33,13 +33,14 @@ function addItem(){
 }
 
 function removeItem(i) {
+    resetInputContainer();
     toDoArray.splice(i, 1);
     localStorage.setItem('ToDoList', JSON.stringify(toDoArray));
     displayToDoList();
 
 }
 
-function editItem(i) {    
+function editItem(i) {
     document.querySelector("#inputBox").value = toDoArray[i].text;
     updateIndex = i;
     console.log("updateIndex: " + updateIndex);
@@ -63,19 +64,23 @@ function updateItem() {
 }
 
 function changeItemStatus(i) {
-    
-    if(document.querySelectorAll(".checkBox")[i].checked) {
-        console.log(document.querySelectorAll(".toDoListText")[i]);
-        document.querySelectorAll(".toDoListDate")[i].style.textDecoration = "line-through";
-        document.querySelectorAll(".toDoListText")[i].style.textDecoration = "line-through";
+
+    console.log(i);
+    console.log(toDoArray);
+    console.log(document.querySelectorAll(".checkBox")[i].checked);
+
+    let dateElement = document.querySelectorAll(".toDoListDate")[i];
+    let textElement = document.querySelectorAll(".toDoListText")[i];
+
+    if (toDoArray[i].status === 'pending') {
         toDoArray[i].status = 'completed';
     } else {
-        document.querySelectorAll(".toDoListDate")[i].style.textDecoration = "none";
-        document.querySelectorAll(".toDoListText")[i].style.textDecoration = "none";
         toDoArray[i].status = 'pending';
     }
+    
     localStorage.setItem('ToDoList', JSON.stringify(toDoArray));
-    //displayToDoList();
+    console.log(toDoArray);
+    displayToDoList();
 
 }
 
@@ -83,12 +88,15 @@ function changeItemStatus(i) {
 function displayToDoList() {
 
     let itemsHTML = '';
-    for(let i=0; i < toDoArray.length; i++) {
+    for (let i = 0; i < toDoArray.length; i++) {
         itemsHTML += `
         <div class="toDoListItem">
-        <div class="toDoListCheckBox"><input type="checkbox" class="checkBox" onclick="changeItemStatus(${i})"></div>
-        <div class="toDoListDate">${toDoArray[i].date}</div>
-        <div class="toDoListText">${toDoArray[i].text}</div>
+        <div class="toDoListCheckBox">
+            <input type="checkbox" class="checkBox" onclick="changeItemStatus(${i})" ${(toDoArray[i].status === 'completed') ? "checked" : ""}>
+            <span class="checkmark"></span>
+        </div>
+        <div class="toDoListDate ${(toDoArray[i].status === 'completed') ? "completed" : ""}  ">${toDoArray[i].date}</div>
+        <div class="toDoListText ${(toDoArray[i].status === 'completed') ? "completed" : ""}">${toDoArray[i].text}</div>
         <div><button class="deleteButton" onclick="removeItem(${i})">Delete</button></div>
         <div><button class="editButton" onclick="editItem(${i})">Edit</button></div>
         </div>
@@ -99,36 +107,77 @@ function displayToDoList() {
 }
 
 
-function sortItems(){
-let sortDirection = localStorage.getItem('SortDirection');
-if (sortDirection === 'asc') {
+function sortItemsDate() {
+    let sortDirection = localStorage.getItem('SortDirection');
+    if (sortDirection === 'asc') {
 
-    toDoArray.sort(function(a,b){
-        // Turn your strings into dates, and then subtract them
-        // to get a value that is either negative, positive, or zero.
-        return new Date(b.date) - new Date(a.date);
-      });
-      sortDirection ='desc';
+        toDoArray.sort(function (a, b) {
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return new Date(b.date) - new Date(a.date);
+        });
+        sortDirection = 'desc';
     } else {
-        toDoArray.sort(function(a,b){
+        toDoArray.sort(function (a, b) {
             return new Date(a.date) - new Date(b.date);
-          });
-          sortDirection = 'asc' 
+        });
+        sortDirection = 'asc'
     }
     console.log(sortDirection);
     localStorage.setItem('SortDirection', sortDirection);
     displayToDoList();
-
 }
 
-function validateInput(){
-let btn = document.querySelector(".addButton");
-let inputBox = document.querySelector("#inputBox");
-    if(inputBox.value === '') {
+function sortItemsText() {
+    let sortDirection = localStorage.getItem('SortDirection');
+    if (sortDirection === 'asc') {
+
+        toDoArray.sort((a, b) => a.text.localeCompare(b.text))
+        sortDirection = 'desc';
+    } else {
+        toDoArray.sort((a, b) => b.text.localeCompare(a.text))
+        sortDirection = 'asc'
+    }
+    console.log(sortDirection);
+    localStorage.setItem('SortDirection', sortDirection);
+    displayToDoList();
+}
+
+function sortItemsStatus() {
+    let sortDirection = localStorage.getItem('SortDirection');
+    if (sortDirection === 'asc') {
+
+        toDoArray.sort((a, b) => a.status.localeCompare(b.status))
+        sortDirection = 'desc';
+    } else {
+        toDoArray.sort((a, b) => b.status.localeCompare(a.status))
+        sortDirection = 'asc'
+    }
+    console.log(sortDirection);
+    localStorage.setItem('SortDirection', sortDirection);
+    displayToDoList();
+}
+
+
+
+function validateInput() {
+    let btn = document.querySelector(".addButton");
+    let inputBox = document.querySelector("#inputBox");
+    if (inputBox.value === '') {
         btn.disabled = true;
     } else {
         btn.disabled = false;
     }
+}
+
+function clearInputText() {
+    document.querySelector("#inputBox").value = '';
+}
+
+function resetInputContainer() {
+    clearInputText();
+    document.querySelector(".addButton").style.display = 'initial';
+    document.querySelector(".updateButton").style.display = 'none';
 }
 
 
