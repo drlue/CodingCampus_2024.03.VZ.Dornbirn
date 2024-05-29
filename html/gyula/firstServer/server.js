9eb5a16ef8e1d67ec114fdf8de0ac1cdcd8e92a6
Gyula Horvath <gylhrvth@gmail.com>
2024-05-29 15:39:52 +0200

    Add MySQL Connection to the web server

--- html/gyula/firstServer/server.js
+++ html/gyula/firstServer/server.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const express = require('express')
+const mysql = require('mysql2/promise')
+
 const app = express()
 const port = 3000
 
@@ -15,6 +18,45 @@ app.get('/api/test', (req, res) => {
   res.send(users)
 })
 
+app.get('/api/country', async (req, res) => {
+    let connection = await connectToDatabase()
+    try {
+        const [results, fields] = await connection.query(
+          'SELECT * FROM country'
+        );
+        
+        res.send(results)
+      } catch (err) {
+        console.log(err);
+        res.sendStatus(404)
+      }
+
+})
+
+
+if (process.env.MYSQL_USER == undefined || process.env.MYSQL_PASSWORD == undefined){
+    console.error('Please create a .env file with MYSQL_USER and MYSQL_PASSWORD')
+    throw new Error('Please create a .env file with MYSQL_USER and MYSQL_PASSWORD')
+}
+
+
+
+
 app.listen(port, () => {
   console.log(`Rest API Server listening on port ${port}`)
 })
+
+let connection = undefined;
+async function connectToDatabase(){
+    if (connection == undefined){
+        connection = await mysql.createConnection({
+            host: 'localhost',
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: 'mondial',
+        });
+        console.log('DB connection established.')
+    } 
+    return connection
+
+}
