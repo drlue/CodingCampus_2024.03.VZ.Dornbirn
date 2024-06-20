285db7250748a7ce4e1e9b0b3650168d4b4af1f2
Irem <aksoy.irem5757@gmail.com>
2024-06-20 09:33:34 +0200

    projekt

--- html/irem/Projekt/script.js
+++ html/irem/Projekt/script.js
@@ -53,7 +53,7 @@ for(let i of products.data){
     //Creat card
     let card=document.createElement("div");
     //Card should have category and should stay hidden initially
-    card.classList.add("card", "i.category","hide");
+    card.classList.add("card", i.category,"hide");
     //image div
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("image-container");
@@ -77,6 +77,66 @@ for(let i of products.data){
 
     card.appendChild(container);
     document.getElementById("products").appendChild(card);
-
-    
 }
+
+//parameter passed from button (parameter same as category)
+function filterProduct(value){
+    //button class code 
+    let buttons = document.querySelectorAll("button-value");
+    buttons.forEach(button => {
+//check if value equals innerText
+if(value.toUpperCase() == button.innerText.toUpperCase()){
+    button.classList.add("active");
+}else{
+    button.classList.remove("active");
+}
+    });
+//select all cards 
+let elements = document.querySelectorAll(".card");
+//loop through all cards
+elements.forEach((element) => {
+    //display all cards on cards 'all' button click 
+if (value == "all"){
+    element.classList.remove("hide");
+}
+else {
+    //check if elemnt contains catogory class 
+    if (element.classList.add(value)){
+        //dsplay elemnt based on catogory
+        element.classList.remove("hide");
+    }
+    else{
+        //hide other elements 
+        element.classList.add("hide");
+
+    }
+  }
+ });
+}
+
+//searach button click
+document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searachInput = document.getElementById ("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    
+    //loop throug all element
+    elements.fCorEach((element,index)=> {
+        //check if text includes the search value
+        if (element.innerText.includes(searachInput.toUpperCase())){
+            //display matching card
+            card[index].classList.remove("hide");
+        }
+        else{
+     //hide others
+     cards[index].classList.add("hide");
+        }
+    })
+})
+
+//initially display all products
+window.onload = () => {
+    filterProduct("all");
+
+};
